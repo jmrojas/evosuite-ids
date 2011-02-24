355b586bd1f135cc3ab2b720d5000eb5fcff607b
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2011-02-24 09:22:44 +0100

    Remove check for visited individuals in tournament selection

---
+++ src/main/java/de/unisb/cs/st/evosuite/ga/OnePlusOneEA.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2010 Saarland University
+ * 
+ * This file is part of the GA library.
+ * 
+ * GA is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * GA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser Public License along with
+ * GA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.unisb.cs.st.evosuite.ga;
+
+/**
+ * (1+1)EA
+ * 
+ * @author Gordon Fraser
+ * 
+ */
+public class OnePlusOneEA extends GeneticAlgorithm {
+
+	/**
+	 * Constructor
+	 * 
+	 * @param factory
+	 */
+	public OnePlusOneEA(ChromosomeFactory factory) {
+		super(factory);
+	}
+
+	private boolean keepOffspring(Chromosome parent, Chromosome offspring) {
+		for (SecondaryObjective objective : secondaryObjectives) {
+			int c = objective.compareChromosomes(parent, offspring);
+			if (c != 0) {
+				return c > 0;
+			}
+		}
+
+		return true;
+	}
+
+	@Override
+	protected void evolve() {
+
+		Chromosome parent = population.get(0);
+		Chromosome offspring = parent.clone();
+
+		notifyMutation(offspring);
+		offspring.mutate();
+
+		fitness_function.getFitness(offspring);
+		notifyEvaluation(offspring);
+
+		// if (replacement_function.keepOffspring(parent, offspring)) {
+		//if (keepOffspring(parent, offspring)) {
+		if (isBetter(offspring, parent)) {
+			logger.debug("Replacing old population");
+			population.set(0, offspring);
+		} else {
+			logger.debug("Keeping old population");
+		}
+		current_iteration++;
+	}
+
+	@Override
+	public void generateSolution() {
+		notifySearchStarted();
+
+		current_iteration = 0;
+
+		// Only one parent
+		generateRandomPopulation(1);
+		fitness_function.getFitness(population.get(0));
+		double fitness = population.get(0).getFitness();
+		logger.info("Initial fitness: " + population.get(0).getFitness());
+
+		while (!isFinished()) {
+			if (getBestIndividual().getFitness() < fitness) {
+				logger.info("Current population: " + getAge());
+				logger.info("Best fitness: " + getBestIndividual().getFitness());
+				fitness = population.get(0).getFitness();
+			}
+			this.notifyIteration();
+			evolve();
+		}
+		notifySearchFinished();
+	}
+}
