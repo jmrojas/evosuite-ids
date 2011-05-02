97444b83e4e9d40c4eaea92a6d9d6a36ab2deab4
Sebastian Steenbuck <sebastian@steenbuck.org>
2011-05-02 16:26:22 +0200

    Added logger messages

---
+++ src/main/java/de/unisb/cs/st/evosuite/coverage/concurrency/ConcurrentTestCase.java
@@ -0,0 +1,363 @@
+/**
+ * 
+ */
+package de.unisb.cs.st.evosuite.coverage.concurrency;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import de.unisb.cs.st.evosuite.assertion.Assertion;
+import de.unisb.cs.st.evosuite.ga.ConstructionFailedException;
+import de.unisb.cs.st.evosuite.testcase.ConstructorStatement;
+import de.unisb.cs.st.evosuite.testcase.MethodStatement;
+import de.unisb.cs.st.evosuite.testcase.Scope;
+import de.unisb.cs.st.evosuite.testcase.StatementInterface;
+import de.unisb.cs.st.evosuite.testcase.TestCase;
+import de.unisb.cs.st.evosuite.testcase.TestFitnessFunction;
+import de.unisb.cs.st.evosuite.testcase.VariableReference;
+
+/**
+ * A concurrentTestCase wraps a BasicTestCase and enriches it with some additional information.
+ *  - the schedule (which is a list of thread ids)
+ *  - a set of all schedulers, which have been given to other objects
+ *  	- used to invalidate said schedulers at appropriate times.
+ *   	- a set of seenThreadIDs. Which is used to generate new thread ids.
+ *   
+ * @author sebastian steenbuck
+ */
+public class ConcurrentTestCase implements TestCase{
+	
+	//A list of thread IDs
+	private final List<Integer> schedule;
+	/*
+	 * Maps statements to schedule.
+	 * That is if a statment 
+	 */
+	private final Map<StatementInterface, Set<Integer>> statementToSchedule;
+	
+	/**
+	 * Holds all schedules, that might in the future become active
+	 */
+	private final Set<Scheduler> generatedSchedules;
+	private final BasicTestCase test;
+	private final Set<Integer> seenThreadIDs;
+
+	public ConcurrentTestCase(BasicTestCase test){
+		assert(test!=null);
+		this.test=test;
+		seenThreadIDs=new HashSet<Integer>();
+		schedule=new ArrayList<Integer>();
+		generatedSchedules = new HashSet<Scheduler>();
+		statementToSchedule = new HashMap<StatementInterface, Set<Integer>>();
+	}
+
+	/**
+	 * Returns the schedule of this thread.
+	 * Note that this schedule is unbounded (new thread ids are generated as needed)
+	 * @return
+	 */
+	public Schedule getSchedule(){
+		Scheduler s = new Scheduler(schedule, seenThreadIDs, generatedSchedules);
+		generatedSchedules.add(s);
+		return s;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return test.hashCode()+schedule.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ConcurrentTestCase other = (ConcurrentTestCase) obj;
+
+		if(!other.test.equals(this.test))
+			return false;
+
+		if(this.schedule.size()!=other.schedule.size())
+			return false;
+
+		for(int i=0;i<schedule.size();i++){
+			if(!this.schedule.get(i).equals(other.schedule.get(i)))
+				return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Create a copy of the test case
+	 */
+	@Override
+	public ConcurrentTestCase clone() {
+		BasicTestCase newTest = test.clone();
+		ConcurrentTestCase newConTest = new ConcurrentTestCase(newTest);
+		newConTest.schedule.addAll(schedule);
+		return newConTest;
+	}
+
+	@Override
+	public int size() {
+		return test.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return test.isEmpty();
+	}
+
+	@Override
+	public void chop(int length) {
+		test.chop(length);
+	}
+
+	@Override
+	public String toCode() {
+		return test.toCode();
+	}
+
+
+	public String getThreadCode(Map<Integer, Throwable> exceptions, int id){
+		
+		StringBuilder b = new StringBuilder();
+		b.append("Integer[] schedule");
+		b.append(id);
+		b.append(" = {");
+		for(Integer i : getSchedule().getContentIterable()){
+			b.append(i);
+			b.append(",");
+		}
+		b.deleteCharAt(b.length()-1);
+		b.append("};");
+		String testString = "private class TestThread"+id+" implements Callable<Void>{ \n"+
+		" private final Triangle param0; \n"+
+		" private final int tid; \n"+
+		" public TestThread"+id+"(Triangle param0, int tid)  {\n"+
+		"	this.param0=param0;\n"+
+		"	this.tid=tid;\n"+
+		" }\n"+
+		"\n"+
+		" @Override\n"+
+		" public Void call() throws Exception {\n"+
+		"	LockRuntime.registerThread(tid);\n"+
+			toCode(exceptions) +
+		"	LockRuntime.threadEnd();\n"+
+		"	return null;\n"+
+		" }\n"+
+		"}\n"+	
+		"\n" +
+		b.toString() + "\n" +
+		"public void test"+id+"(){\n"+
+		"	Triangle var0 = new Triangle();\n"+
+		"	FutureTask<Void> c = new FutureTask<Void>(new ControllerRuntime(new SimpleScheduler(schedule"+id+"), " + ConcurrencyCoverageFactory.THREAD_COUNT + "));\n"+
+		"Set<FutureTask<Void>> testFutures = new HashSet<FutureTask<Void>>();\n" +
+		"for(int i=0 ; i<" + ConcurrencyCoverageFactory.THREAD_COUNT + " ; i++){\n" +
+		"	FutureTask<Void> testFuture = new FutureTask<Void>(new TestThread"+id+"(var0,i));\n"+
+		"	Thread testThread = new Thread(testFuture);\n" +
+		"	testThread.start();\n" +
+		"	testFutures.add(testFuture);\n" +
+		"}\n\n" +
+		
+		"	try{\n\n"+		
+		"		for(FutureTask<Void> testFuture : testFutures){\n"+
+		"		testFuture.get();\n"+
+		"		}\n\n"+
+		"    c.get();\n"+
+		"	}catch(Exception e){\n"+
+		"    e.printStackTrace();\n"+
+		"	}";
+		
+		return testString;
+	}
+
+
+	@Override
+	public String toCode(Map<Integer, Throwable> exceptions) {
+		return test.toCode(exceptions);
+	}
+
+	@Override
+	public List<VariableReference> getObjects(Type type, int position) {
+		return test.getObjects(type,position);
+	}
+
+	@Override
+	public List<VariableReference> getObjects(int position) {
+		return test.getObjects(position);
+	}
+
+	@Override
+	public VariableReference getRandomObject() {
+		return test.getRandomObject();
+	}
+
+	@Override
+	public VariableReference getRandomObject(int position) {
+		return test.getRandomObject(position);
+	}
+
+	@Override
+	public VariableReference getRandomObject(Type type)
+	throws ConstructionFailedException {
+		return test.getRandomObject(type);
+	}
+
+	@Override
+	public VariableReference getRandomObject(Type type, int position)
+	throws ConstructionFailedException {
+		return test.getRandomObject(type, position);
+	}
+
+	@Override
+	public Object getObject(VariableReference reference, Scope scope) {
+		return test.getObject(reference, scope);
+	}
+
+	@Override
+	public void renameVariable(int old_position, int new_position) {
+		test.renameVariable(old_position, new_position);
+	}
+
+	@Override
+	public VariableReference setStatement(StatementInterface statement, int position) {
+		return test.setStatement(statement, position);
+	}
+
+	@Override
+	public void addStatement(StatementInterface statement, int position) {
+		test.addStatement(statement, position);
+	}
+
+
+	@Override
+	public void addStatement(StatementInterface statement) {
+		if(statement instanceof ConstructorStatement ||
+				statement instanceof MethodStatement){
+			statement=new ScheduleLogWrapper(statement);
+		}
+		test.addStatement(statement);
+	}
+
+	@Override
+	public VariableReference getReturnValue(int position) {
+		return test.getReturnValue(position);
+	}
+
+	@Override
+	public boolean hasReferences(VariableReference var) {
+		return test.hasReferences(var);
+	}
+
+	@Override
+	public List<VariableReference> getReferences(VariableReference var) {
+		return test.getReferences(var);
+	}
+
+	@Override
+	public void remove(int position) {
+		test.remove(position);
+	}
+
+	@Override
+	public StatementInterface getStatement(int position) {
+		return test.getStatement(position);
+	}
+
+	@Override
+	public boolean hasObject(Type type, int position) {
+		return test.hasObject(type, position);
+	}
+
+	@Override
+	public boolean hasCastableObject(Type type) {
+		return test.hasCastableObject(type);
+	}
+
+	@Override
+	public Set<Class<?>> getAccessedClasses() {
+		return test.getAccessedClasses();
+	}
+
+	@Override
+	public boolean hasAssertions() {
+		return test.hasAssertions();
+	}
+
+	@Override
+	public List<Assertion> getAssertions() {
+		return test.getAssertions();
+	}
+
+	@Override
+	public void removeAssertions() {
+		test.removeAssertions();
+	}
+
+	@Override
+	public boolean isValid() {
+		return test.isValid();
+	}
+
+	public Set<Class<?>> getDeclaredExceptions() {
+		return test.getDeclaredExceptions();
+	}
+
+	@Override
+	public boolean hasCalls() {
+		return test.hasCalls();
+	}
+
+	@Override
+	public void addCoveredGoal(TestFitnessFunction goal) {
+		test.addCoveredGoal(goal);
+	}
+
+	@Override
+	public Set<TestFitnessFunction> getCoveredGoals() {
+		return test.getCoveredGoals();
+	}
+
+	@Override
+	public Iterator<StatementInterface> iterator() {
+		return test.iterator();
+	}
+
+	/* (non-Javadoc)
+	 * @see de.unisb.cs.st.evosuite.testcase.TestCase#addAssertions(de.unisb.cs.st.evosuite.testcase.TestCase)
+	 */
+	@Override
+	public void addAssertions(TestCase other) {
+		test.addAssertions(other);
+	}
+
+	/* (non-Javadoc)
+	 * @see de.unisb.cs.st.evosuite.testcase.TestCase#getStatements()
+	 */
+	@Override
+	public List<StatementInterface> getStatements() {
+		return test.getStatements();
+	}
+
+	/* (non-Javadoc)
+	 * @see de.unisb.cs.st.evosuite.testcase.TestCase#isPrefix(de.unisb.cs.st.evosuite.testcase.TestCase)
+	 */
+	@Override
+	public boolean isPrefix(TestCase t) {
+		return test.isPrefix(t);
+	}
+
+
+}
---
+++ src/main/java/de/unisb/cs/st/evosuite/testcase/TestCaseExecutor.java
@@ -0,0 +1,360 @@
+/*
+ * Copyright (C) 2010 Saarland University
+ * 
+ * This file is part of EvoSuite.
+ * 
+ * EvoSuite is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser Public License along with
+ * EvoSuite. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.unisb.cs.st.evosuite.testcase;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.apache.log4j.Logger;
+
+import de.unisb.cs.st.evosuite.Properties;
+import de.unisb.cs.st.evosuite.contracts.ContractChecker;
+import de.unisb.cs.st.evosuite.coverage.concurrency.ConcurrentTestRunnable;
+import de.unisb.cs.st.evosuite.ga.stoppingconditions.MaxStatementsStoppingCondition;
+import de.unisb.cs.st.evosuite.ga.stoppingconditions.MaxTestsStoppingCondition;
+import de.unisb.cs.st.evosuite.sandbox.Sandbox;
+
+/**
+ * The test case executor manages thread creation/deletion to execute a test
+ * case
+ * 
+ * @author Gordon Fraser
+ * 
+ */
+public class TestCaseExecutor implements ThreadFactory {
+
+	private static final Logger logger = Logger.getLogger(TestCaseExecutor.class);
+
+	private boolean log = true;
+
+	public static long timeout = Properties.getPropertyOrDefault("timeout", 5000);
+
+	public static boolean cpuTimeout = Properties.getPropertyOrDefault("cpu_timeout",
+	                                                                   false);
+
+	private static boolean logTimeout = Properties.getPropertyOrDefault("log_timeout",
+	                                                                    false);
+
+	private static final PrintStream systemOut = System.out;
+	private static final PrintStream systemErr = System.err;
+
+	private static TestCaseExecutor instance = null;
+
+	private ExecutorService executor;
+
+	private Thread currentThread = null;
+
+	//private static ExecutorService executor = Executors.newCachedThreadPool();
+
+	private List<ExecutionObserver> observers;
+
+	protected boolean static_hack = Properties.getPropertyOrDefault("static_hack", false);
+
+	public static TestCaseExecutor getInstance() {
+		if (instance == null)
+			instance = new TestCaseExecutor();
+
+		return instance;
+	}
+
+	/**
+	 * Execute a test case
+	 * 
+	 * @param test
+	 *            The test case to execute
+	 * @param mutant
+	 *            The mutation to active (null = no mutation)
+	 * 
+	 * @return Result of the execution
+	 */
+	public static ExecutionResult runTest(TestCase test) {
+
+		ExecutionResult result = new ExecutionResult(test, null);
+
+		try {
+			TestCaseExecutor executor = getInstance();
+			logger.debug("Executing test");
+			result = executor.execute(test);
+
+			int num = test.size();
+			MaxStatementsStoppingCondition.statementsExecuted(num);
+
+			// for(TestObserver observer : observers) {
+			// observer.testResult(result);
+			// }
+		} catch (Exception e) {
+			System.out.println("TG: Exception caught: " + e);
+			e.printStackTrace();
+			logger.fatal("TG: Exception caught: ", e);
+			System.exit(1);
+		}
+
+		// System.out.println("TG: Killed "+result.getNumKilled()+" out of "+mutants.size());
+		return result;
+	}
+
+	private TestCaseExecutor() {
+		executor = Executors.newSingleThreadExecutor(this);
+		newObservers();
+	}
+
+	public static class TimeoutExceeded extends RuntimeException {
+		private static final long serialVersionUID = -5314228165430676893L;
+	}
+
+	public void setup() {
+		// start own thread
+	}
+
+	public static void pullDown() {
+		if (instance != null)
+			instance.executor.shutdownNow();
+	}
+
+	public void setLogging(boolean value) {
+		log = value;
+	}
+
+	public void addObserver(ExecutionObserver observer) {
+		observers.add(observer);
+	}
+
+	public void removeObserver(ExecutionObserver observer) {
+		observers.remove(observer);
+	}
+
+	public void newObservers() {
+		observers = new ArrayList<ExecutionObserver>();
+		if (Properties.CHECK_CONTRACTS) {
+			observers.add(new ContractChecker());
+		}
+
+	}
+
+	private void resetObservers() {
+		for (ExecutionObserver observer : observers) {
+			observer.clear();
+		}
+	}
+
+	public ExecutionResult execute(TestCase tc) {
+		Scope scope = new Scope();
+		return execute(tc, scope);
+	}
+
+	public ExecutionResult execute(TestCase tc, Scope scope) {
+		ExecutionTracer.getExecutionTracer().clear();
+		if (static_hack)
+			TestCluster.getInstance().resetStaticClasses();
+		resetObservers();
+		ExecutionObserver.currentTest(tc);
+		MaxTestsStoppingCondition.testExecuted();
+
+		TimeoutHandler<ExecutionResult> handler = new TimeoutHandler<ExecutionResult>();
+
+		//#TODO steenbuck could be nicer (TestRunnable should be an interface
+		InterfaceTestRunnable callable;
+		if (Properties.CRITERION.equals(Properties.CRITERIA.CONCURRENCY)) {
+			callable = new ConcurrentTestRunnable(tc, scope, observers);
+		} else {
+			callable = new TestRunnable(tc, scope, observers);
+		}
+		//FutureTask<ExecutionResult> task = new FutureTask<ExecutionResult>(callable);
+		//executor.execute(task);
+
+		try {
+			//ExecutionResult result = task.get(timeout, TimeUnit.MILLISECONDS);
+			ExecutionResult result = handler.execute(callable, executor, timeout,
+			                                         cpuTimeout);
+			return result;
+		} catch (ThreadDeath t) {
+			logger.warn("Caught ThreadDeath during test execution");
+			Sandbox.tearDownEverything();
+			ExecutionResult result = new ExecutionResult(tc, null);
+			result.exceptions = callable.getExceptionsThrown();
+			result.trace = ExecutionTracer.getExecutionTracer().getTrace();
+			ExecutionTracer.getExecutionTracer().clear();
+			return result;
+
+		} catch (InterruptedException e1) {
+			Sandbox.tearDownEverything();
+			logger.info("InterruptedException");
+			ExecutionResult result = new ExecutionResult(tc, null);
+			result.exceptions = callable.getExceptionsThrown();
+			result.trace = ExecutionTracer.getExecutionTracer().getTrace();
+			ExecutionTracer.getExecutionTracer().clear();
+			return result;
+		} catch (ExecutionException e1) {
+			if (e1.getCause() instanceof AssertionError
+			        && e1.getCause().getStackTrace()[0].getClassName().contains("de.unisb.cs.st.evosuite")) {
+				//e1.printStackTrace();
+				logger.warn("Assertion Error in evosuitecode", e1);
+				throw (AssertionError) e1.getCause();
+			}
+			Sandbox.tearDownEverything();
+			e1.printStackTrace();
+			logger.info("ExecutionException");
+			ExecutionResult result = new ExecutionResult(tc, null);
+			result.exceptions = callable.getExceptionsThrown();
+			result.trace = ExecutionTracer.getExecutionTracer().getTrace();
+			ExecutionTracer.getExecutionTracer().clear();
+			return result;
+		} catch (TimeoutException e1) {
+			Sandbox.tearDownEverything();
+			System.setOut(systemOut);
+			System.setErr(systemErr);
+
+			if (logTimeout) {
+				System.err.println("Timeout occurred for " + Properties.TARGET_CLASS);
+			}
+			logger.info("TimeoutException, need to stop runner");
+			ExecutionTracer.setKillSwitch(true);
+			ExecutionTracer.disable();
+			//task.cancel(true);
+			handler.getLastTask().cancel(true);
+
+			if (!callable.isRunFinished()) {
+				logger.info("Run not finished, waiting...");
+				try {
+					executor.awaitTermination(timeout, TimeUnit.MILLISECONDS);
+				} catch (InterruptedException e) {
+					logger.info("Interrupted");
+					e.printStackTrace();
+				}
+				if (!callable.isRunFinished()) {
+					logger.info("Run still not finished, replacing executor.");
+					try {
+						executor.shutdownNow();
+						if (currentThread.isAlive()) {
+							logger.warn("Thread survived - unsafe operation.");
+							for (StackTraceElement element : currentThread.getStackTrace()) {
+								logger.warn(element);
+							}
+							currentThread.stop();
+						}
+					} catch (ThreadDeath t) {
+						logger.info("ThreadDeath.");
+					} catch (Throwable t) {
+						logger.info("Throwable: " + t);
+					}
+					executor = Executors.newSingleThreadExecutor(this);
+				}
+			}
+			ExecutionResult result = new ExecutionResult(tc, null);
+			result.exceptions = callable.getExceptionsThrown();
+			result.exceptions.put(tc.size(), new TestCaseExecutor.TimeoutExceeded());
+			result.trace = ExecutionTracer.getExecutionTracer().getTrace();
+			ExecutionTracer.getExecutionTracer().clear();
+			ExecutionTracer.setKillSwitch(false);
+			ExecutionTracer.enable();
+
+			return result;
+		}
+	}
+
+	@SuppressWarnings("deprecation")
+	public Map<Integer, Throwable> run(TestCase tc, Scope scope) {
+		// StringTraceExecutionObserver obs = new
+		// StringTraceExecutionObserver();
+		// observers.add(obs);
+		ExecutionTracer.getExecutionTracer().clear();
+		if (static_hack)
+			TestCluster.getInstance().resetStaticClasses();
+		resetObservers();
+		ExecutionObserver.currentTest(tc);
+
+		TestRunner runner = new TestRunner(null);
+		runner.setLogging(log);
+		runner.setup(tc, scope, observers);
+		MaxTestsStoppingCondition.testExecuted();
+		// MaxStatementsStoppingCondition.statementsExecuted(tc.size());
+
+		try {
+			// Start the test.
+			runner.start();
+
+			// If test doesn't finish in time, suspend it.
+			runner.join(timeout);
+
+			if (!runner.runFinished) {
+				logger.warn("Exceeded max wait (" + timeout + "ms): aborting test input:");
+				logger.warn(tc.toCode());
+				runner.interrupt();
+
+				if (runner.isAlive()) {
+					// If test doesn't finish in time, suspend it.
+					logger.info("Thread ignored interrupt, using killswitch");
+					ExecutionTracer.setKillSwitch(true);
+					runner.join(timeout / 2);
+					if (!runner.runFinished) {
+						logger.info("Trying thread.stop()");
+						for (StackTraceElement element : runner.getStackTrace()) {
+							logger.info(element.toString());
+						}
+						runner.stop();// We use this deprecated method because
+						              // it's the only way to
+						// stop a thread no matter what it's doing.
+						// return runner.exceptionsThrown;
+						runner.join(timeout / 2);
+
+						if (runner.isAlive()) {
+							logger.warn("Thread ignored stop()! All is lost!");
+							for (StackTraceElement element : runner.getStackTrace()) {
+								logger.warn(element.toString());
+							}
+						}
+					}
+
+					ExecutionTracer.enable();
+				}
+				ExecutionTracer.getExecutionTracer().clear();
+				ExecutionTracer.setKillSwitch(false);
+				runner.exceptionsThrown.put(tc.size(),
+				                            new TestCaseExecutor.TimeoutExceeded());
+			}
+			return runner.exceptionsThrown;
+
+		} catch (java.lang.InterruptedException e) {
+			throw new IllegalStateException(
+			        "A RunnerThread thread shouldn't be interrupted by anyone! "
+			                + "(this may be a bug in the program; please report it.)");
+		}
+
+	}
+
+	public Map<Integer, Throwable> run(TestCase tc) {
+		Scope scope = new Scope();
+		return run(tc, scope);
+	}
+
+	@Override
+	public Thread newThread(Runnable r) {
+		currentThread = new Thread(r);
+		ExecutionTracer.setThread(currentThread);
+		return currentThread;
+	}
+}
