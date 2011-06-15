7bd140185f550b197623efca8aca91a4794ce41a
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2011-06-15 17:41:21 +0200

    LCSAJ fixes: Add instrumentation for non-branches at LCSAJ begin / end Adapt LCSAJ to refactored Branch classes

---
+++ src/main/java/de/unisb/cs/st/evosuite/javaagent/BytecodeInstrumentation.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright (C) 2010 Saarland University
+ * 
+ * This file is part of EvoSuite.
+ * 
+ * EvoSuite is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser Public License along with
+ * EvoSuite. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.unisb.cs.st.evosuite.javaagent;
+
+import java.io.PrintWriter;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.security.ProtectionDomain;
+
+import org.apache.log4j.Logger;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.util.CheckClassAdapter;
+import org.objectweb.asm.util.TraceClassVisitor;
+
+import de.unisb.cs.st.evosuite.Properties;
+import de.unisb.cs.st.evosuite.Properties.Criterion;
+import de.unisb.cs.st.evosuite.cfg.CFGClassAdapter;
+import de.unisb.cs.st.evosuite.primitives.PrimitiveClassAdapter;
+import de.unisb.cs.st.javalanche.coverage.distance.Hierarchy;
+
+/**
+ * The bytecode transformer - transforms bytecode depending on package and
+ * whether it is the class under test
+ * 
+ * @author Gordon Fraser
+ * 
+ */
+public class BytecodeInstrumentation implements ClassFileTransformer {
+
+	private static Hierarchy hierarchy;
+
+	private static final boolean MUTATION = Properties.CRITERION == Criterion.MUTATION;
+
+	static {
+		if (Properties.INSTRUMENT_PARENT)
+			hierarchy = Hierarchy.readFromDefaultLocation();
+	}
+
+	protected static Logger logger = Logger.getLogger(BytecodeInstrumentation.class);
+
+	// private static RemoveSystemExitTransformer systemExitTransformer = new
+	// RemoveSystemExitTransformer();
+
+	private static String target_class = Properties.TARGET_CLASS;
+
+	private boolean isTargetClass(String className) {
+		if (className.equals(target_class) || className.startsWith(target_class + "$")) {
+			return true;
+		}
+
+		if (Properties.INSTRUMENT_PARENT) {
+			return hierarchy.getAllSupers(target_class).contains(className);
+		}
+
+		return false;
+	}
+
+	static {
+		logger.info("Loading bytecode transformer for " + Properties.PROJECT_PREFIX);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * java.lang.instrument.ClassFileTransformer#transform(java.lang.ClassLoader
+	 * , java.lang.String, java.lang.Class, java.security.ProtectionDomain,
+	 * byte[])
+	 */
+	@Override
+	public byte[] transform(ClassLoader loader, String className,
+	        Class<?> classBeingRedefined, ProtectionDomain protectionDomain,
+	        byte[] classfileBuffer) throws IllegalClassFormatException {
+
+		if (className != null) {
+			try {
+				String classNameWithDots = className.replace('/', '.');
+
+				// Some packages we shouldn't touch - hard-coded
+				if (!classNameWithDots.startsWith(Properties.PROJECT_PREFIX)
+				        && (classNameWithDots.startsWith("java")
+				                || classNameWithDots.startsWith("sun")
+				                || classNameWithDots.startsWith("org.aspectj.org.eclipse") || classNameWithDots.startsWith("org.mozilla.javascript.gen.c"))) {
+					return classfileBuffer;
+				}
+				if (classNameWithDots.startsWith(Properties.PROJECT_PREFIX)) {
+
+					// logger.debug("Removing calls to System.exit() from class: "
+					// + classNameWithDots);
+					// classfileBuffer = systemExitTransformer
+					// .transformBytecode(classfileBuffer);
+
+					ClassReader reader = new ClassReader(classfileBuffer);
+					ClassWriter writer = new ClassWriter(
+					        org.objectweb.asm.ClassWriter.COMPUTE_MAXS);
+
+					ClassVisitor cv = writer;
+
+					// Print out bytecode if debug is enabled
+					// if(logger.isDebugEnabled())
+					// cv = new TraceClassVisitor(cv, new
+					// PrintWriter(System.out));
+
+					// Apply transformations to class under test and its owned
+					// classes
+					if (isTargetClass(classNameWithDots)) {
+						cv = new AccessibleClassAdapter(cv, className);
+						cv = new ExecutionPathClassAdapter(cv, className);
+						cv = new CFGClassAdapter(cv, className);
+
+					} else if (Properties.MAKE_ACCESSIBLE) {
+						// Convert protected/default access to public access
+						cv = new AccessibleClassAdapter(cv, className);
+					}
+
+					// Collect constant values for the value pool
+					if (!MUTATION) {
+						cv = new PrimitiveClassAdapter(cv, className);
+					}
+
+					// If we need to reset static constructors, make them
+					// explicit methods
+					if (Properties.STATIC_HACK)
+						cv = new StaticInitializationClassAdapter(cv, className);
+
+					if (Properties.TT) {
+						logger.info("Transforming " + className);
+						ClassNode cn = new ClassNode();
+						reader.accept(cn, ClassReader.SKIP_FRAMES);
+						TestabilityTransformation tt = new TestabilityTransformation(cn);
+						// cv = new TraceClassVisitor(writer, new
+						// PrintWriter(System.out));
+						cv = new TraceClassVisitor(cv, new PrintWriter(System.out));
+						cv = new CheckClassAdapter(cv);
+						tt.transform().accept(cv);
+					} else {
+						reader.accept(cv, ClassReader.SKIP_FRAMES);
+					}
+					// Print out bytecode if debug is enabled
+					// if(logger.isDebugEnabled())
+					// cv = new TraceClassVisitor(cv, new
+					// PrintWriter(System.out));
+					classfileBuffer = writer.toByteArray();
+
+					return classfileBuffer;
+
+				}
+
+			} catch (Throwable t) {
+				logger.fatal("Transformation of class " + className + " failed", t);
+				// TODO why all the redundant printStackTrace()s?
+
+				//				StringWriter writer = new StringWriter();
+				//				t.printStackTrace(new PrintWriter(writer));
+				//				logger.fatal(writer.getBuffer().toString());
+				// LogManager.shutdown();
+				System.out.flush();
+				System.exit(0);
+				// throw new RuntimeException(e.getMessage());
+			}
+		}
+		return classfileBuffer;
+
+	}
+
+}
---
+++ src/main/java/de/unisb/cs/st/evosuite/setup/ScanProject.java
@@ -0,0 +1,324 @@
+/*
+ * Copyright (C) 2010 Saarland University
+ * 
+ * This file is part of EvoSuite.
+ * 
+ * EvoSuite is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser Public License along with
+ * EvoSuite. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.unisb.cs.st.evosuite.setup;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
+
+import org.eclipse.jdt.core.dom.Modifier;
+import org.objectweb.asm.ClassReader;
+
+import de.unisb.cs.st.evosuite.Properties;
+import de.unisb.cs.st.evosuite.classcreation.ClassFactory;
+import de.unisb.cs.st.evosuite.javaagent.TestSuitePreMain;
+import de.unisb.cs.st.evosuite.utils.Utils;
+
+/**
+ * @author Gordon Fraser
+ * 
+ */
+public class ScanProject {
+
+	public final static class ZipClassLoader extends ClassLoader {
+		private final ZipFile file;
+
+		public ZipClassLoader(String filename) throws IOException {
+			this.file = new ZipFile(filename);
+		}
+
+		public ZipClassLoader(File file) throws IOException {
+			this.file = new ZipFile(file);
+		}
+
+		@Override
+		protected Class<?> findClass(String name) throws ClassNotFoundException {
+
+			ZipEntry entry = this.file.getEntry(name.replace('.', '/') + ".class");
+			if (entry == null) {
+				throw new ClassNotFoundException(name);
+			}
+			try {
+				byte[] array = new byte[(int) entry.getSize()];
+				InputStream in = this.file.getInputStream(entry);
+				ByteArrayOutputStream out = new ByteArrayOutputStream(array.length);
+				int length = in.read(array);
+				while (length > 0) {
+					out.write(array, 0, length);
+					length = in.read(array);
+				}
+				ClassReader reader = new ClassReader(array);
+				return defineClass(reader.getClassName().replace("/", "."),
+				                   out.toByteArray(), 0, out.size());
+			} catch (IOException exception) {
+				throw new ClassNotFoundException(name, exception);
+			}
+		}
+	}
+
+	public final static class FileClassLoader extends ClassLoader {
+
+		@Override
+		protected Class<?> findClass(String name) throws ClassNotFoundException {
+			File file = new File(name);
+			try {
+				byte[] array = new byte[(int) file.length()];
+				InputStream in = new FileInputStream(file);
+				ByteArrayOutputStream out = new ByteArrayOutputStream(array.length);
+				int length = in.read(array);
+				while (length > 0) {
+					out.write(array, 0, length);
+					length = in.read(array);
+				}
+				ClassReader reader = new ClassReader(array);
+				return defineClass(reader.getClassName().replace("/", "."),
+				                   out.toByteArray(), 0, out.size());
+			} catch (IOException exception) {
+				throw new ClassNotFoundException(name, exception);
+			}
+		}
+	}
+
+	private static Set<Class<?>> findClasses(File directory, String packageName)
+	        throws ClassNotFoundException {
+		Set<Class<?>> classes = new HashSet<Class<?>>();
+		System.out.println("* Searching in: " + directory);
+		if (!directory.exists()) {
+			return classes;
+		}
+		File[] files = directory.listFiles();
+		for (File file : files) {
+			System.out.println("* Found class file: " + file);
+			if (file.isDirectory()) {
+				assert !file.getName().contains(".");
+				classes.addAll(findClasses(file, packageName + "." + file.getName()));
+			} else if (file.getName().endsWith(".class")) {
+				if (Properties.STUBS) {
+					Class<?> clazz = Class.forName(packageName + '.'
+					        + file.getName().substring(0, file.getName().length() - 6));
+
+					if (Modifier.isAbstract(clazz.getModifiers()) && !clazz.isInterface()) {
+						ClassFactory cf = new ClassFactory();
+						Class<?> stub = cf.createClass(clazz);
+						if (stub != null)
+							classes.add(stub);
+					}
+				}
+				try {
+					classes.add(Class.forName(packageName + '.'
+					        + file.getName().substring(0, file.getName().length() - 6)));
+				} catch (IllegalAccessError e) {
+					System.out.println("Cannot access class " + packageName + '.'
+					        + file.getName().substring(0, file.getName().length() - 6));
+				} catch (NoClassDefFoundError e) {
+					System.out.println("Cannot find class " + packageName + '.'
+					        + file.getName().substring(0, file.getName().length() - 6));
+				} catch (ExceptionInInitializerError e) {
+					System.out.println("Exception in initializer of " + packageName + '.'
+					        + file.getName().substring(0, file.getName().length() - 6));
+				}
+			}
+		}
+		return classes;
+	}
+
+	private static Set<Class<?>> loadClass(File file, String packageName)
+	        throws ClassNotFoundException {
+		Set<Class<?>> set = new HashSet<Class<?>>();
+		if (file.isDirectory()) {
+			assert !file.getName().contains(".");
+			set.addAll(findClasses(file, packageName + "." + file.getName()));
+		} else if (file.getName().endsWith(".class")) {
+			System.out.println("    "
+			        + file.toString().replace(".class", "").replace("/", "."));
+			if (Properties.STUBS) {
+				Class<?> clazz = Class.forName(file.toString().replace(".class", "").replace("/",
+				                                                                             "."));
+				if (Modifier.isAbstract(clazz.getModifiers()) && !clazz.isInterface()) {
+					ClassFactory cf = new ClassFactory();
+					Class<?> stub = cf.createClass(clazz);
+				}
+			}
+			try {
+				set.add(Class.forName(file.toString().replace(".class", "").replace("/",
+				                                                                    ".")));
+			} catch (IllegalAccessError e) {
+				System.out.println("Cannot access class " + packageName + '.'
+				        + file.getName().substring(0, file.getName().length() - 6));
+			} catch (NoClassDefFoundError e) {
+				System.out.println("Cannot find class " + packageName + '.'
+				        + file.getName().substring(0, file.getName().length() - 6));
+			} catch (ExceptionInInitializerError e) {
+				System.out.println("Exception in initializer of " + packageName + '.'
+				        + file.getName().substring(0, file.getName().length() - 6));
+			}
+		}
+		return set;
+	}
+
+	/**
+	 * Analyze all classes of a given package prefix that can be found in the
+	 * classpath
+	 */
+	private static Set<Class<?>> getClasses(String packageName)
+	        throws ClassNotFoundException, IOException {
+		Set<Class<?>> set = new HashSet<Class<?>>();
+
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		assert classLoader != null;
+		Collection<String> list = ResourceList.getResources(Pattern.compile(packageName
+		        + "/.*\\.class$"));
+		for (String name : list) {
+			set.addAll(loadClass(new File(name), packageName));
+		}
+
+		return set;
+	}
+
+	/**
+	 * Analyze all classes that can be found in a given directory
+	 * 
+	 * @param directory
+	 * @throws ClassNotFoundException
+	 */
+	private static Set<Class<?>> getClasses(File directory) throws ClassNotFoundException {
+		if (directory.getName().endsWith(".jar")) {
+			return getClassesJar(directory);
+		} else if (directory.getName().endsWith(".class")) {
+			Set<Class<?>> set = new HashSet<Class<?>>();
+			try {
+				System.out.println("* Loading class " + directory.getName());
+				set.add(new FileClassLoader().findClass(directory.getPath()));
+			} catch (IllegalAccessError e) {
+				System.out.println("Cannot access class "
+				        + directory.getName().substring(0,
+				                                        directory.getName().length() - 6));
+			} catch (NoClassDefFoundError e) {
+				System.out.println("Cannot find class "
+				        + directory.getName().substring(0,
+				                                        directory.getName().length() - 6)
+				        + " in path " + directory);
+			} catch (ExceptionInInitializerError e) {
+				System.out.println("Exception in initializer of "
+				        + directory.getName().substring(0,
+				                                        directory.getName().length() - 6));
+			}
+			return set;
+		} else if (directory.isDirectory()) {
+			Set<Class<?>> set = new HashSet<Class<?>>();
+			for (File file : directory.listFiles()) {
+				set.addAll(getClasses(file));
+			}
+			return set;
+		} else {
+			return new HashSet<Class<?>>();
+			//			throw new RuntimeException("Don't know how to handle: " + directory);
+		}
+	}
+
+	private static Set<Class<?>> getClassesJar(File file) throws ClassNotFoundException {
+
+		Set<Class<?>> set = new HashSet<Class<?>>();
+
+		ZipFile zf;
+		ZipClassLoader zcl;
+		try {
+			zcl = new ZipClassLoader(file);
+			zf = new ZipFile(file);
+		} catch (final ZipException e) {
+			throw new Error(e);
+		} catch (final IOException e) {
+			throw new Error(e);
+		}
+		@SuppressWarnings("rawtypes")
+		final Enumeration e = zf.entries();
+		while (e.hasMoreElements()) {
+			final ZipEntry ze = (ZipEntry) e.nextElement();
+			final String fileName = ze.getName();
+			if (!fileName.endsWith(".class"))
+				continue;
+
+			try {
+				System.out.println("* Loading class " + fileName + " from jar file");
+				set.add(zcl.findClass(fileName.replace(".class", "")));
+				//				Class.forName(fileName.replace(".class", "").replace("/", "."));
+			} catch (IllegalAccessError ex) {
+				System.out.println("Cannot access class "
+				        + file.getName().substring(0, file.getName().length() - 6));
+			} catch (NoClassDefFoundError ex) {
+				System.out.println("Cannot find class "
+				        + file.getName().substring(0, file.getName().length() - 6));
+			} catch (ExceptionInInitializerError ex) {
+				System.out.println("Exception in initializer of "
+				        + file.getName().substring(0, file.getName().length() - 6));
+			}
+		}
+		try {
+			zf.close();
+		} catch (final IOException e1) {
+			throw new Error(e1);
+		}
+		return set;
+
+	}
+
+	/**
+	 * Entry point - generate task files
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//System.out.println("Scanning project for test suite generation.");
+		Set<Class<?>> classes = new HashSet<Class<?>>();
+		try {
+			if (Properties.PROJECT_PREFIX != null) {
+				System.out.println("* Analyzing project prefix: "
+				        + Properties.PROJECT_PREFIX);
+				classes.addAll(getClasses(Properties.PROJECT_PREFIX));
+			} else if (args.length > 0) {
+				for (String arg : args) {
+					System.out.println("* Analyzing project directory: " + arg);
+					classes.addAll(getClasses(new File(arg)));
+				}
+			} else {
+				System.out.println("* Please specify either project prefix or directory");
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		TestSuitePreMain.distanceTransformer.saveData();
+		Utils.addURL(ClassFactory.getStubDir() + "/classes/");
+		TestTaskGenerator.hierarchy.calculateSubclasses();
+		System.out.println("* Creating test files");
+		//		TestTaskGenerator.suggestTasks(Properties.PROJECT_PREFIX);
+		TestTaskGenerator.suggestTasks(classes);
+	}
+}
