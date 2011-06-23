497caff35fdb57b3f098b5762c835f09c1592100
lang <devnull@localhost>
2011-06-23 04:04:58 +0200

    Added the possibility to draw LCSAJ Graphs. This means for each LCSAJ the RawControlFlowGraph is marked additionally with an edge from the start to the end node. Also it is possible to track wrong taken jumps during the fitness evaluation. Right and wrong jumps are marked with two different colors.
    Implementation done with the nasty graphViz library ;-)
    
    But keep in mind: The algorithms are very inefficent!!! Will be fixed soon....

---
+++ src/main/java/de/unisb/cs/st/evosuite/assertion/InspectorTraceObserver.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2010 Saarland University
+ * 
+ * This file is part of EvoSuite.
+ * 
+ * EvoSuite is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser Public License along with
+ * EvoSuite. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.unisb.cs.st.evosuite.assertion;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import de.unisb.cs.st.evosuite.testcase.ExecutionObserver;
+import de.unisb.cs.st.evosuite.testcase.MethodStatement;
+import de.unisb.cs.st.evosuite.testcase.Scope;
+import de.unisb.cs.st.evosuite.testcase.StatementInterface;
+import de.unisb.cs.st.evosuite.testcase.VariableReference;
+
+public class InspectorTraceObserver extends ExecutionObserver {
+	private final static Logger logger = Logger.getLogger(InspectorTraceObserver.class);
+
+	private final InspectorManager manager = InspectorManager.getInstance();
+
+	private final InspectorTrace trace = new InspectorTrace();
+
+	@Override
+	public void clear() {
+		trace.clear();
+	}
+
+	@Override
+	public void output(int position, String output) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void statement(StatementInterface statement, Scope scope, Throwable exception) {
+		VariableReference retval = statement.getReturnValue();
+
+		if (retval == null)
+			return;
+
+		// Add inspector calls on return value
+		List<Inspector> inspectors = manager.getInspectors(retval.getVariableClass());
+		if (retval.getObject(scope) != null && !inspectors.isEmpty()) {
+			List<Object> result = new ArrayList<Object>();
+			for (Inspector i : inspectors) {
+				try {
+					Object value = i.getValue(retval.getObject(scope));
+					result.add(value);
+					// TODO: Need to keep reference to inspector if exception is thrown!
+				} catch (IllegalArgumentException e) {
+					logger.info("Exception during call to inspector: " + e);
+					continue;
+				} catch (IllegalAccessException e) {
+					logger.info("Exception during call to inspector: " + e);
+					continue;
+				} catch (InvocationTargetException e) {
+					logger.info("Exception during call to inspector: " + e.getCause());
+					continue;
+				}
+			}
+
+			trace.inspector_results.put(statement.getPosition(), result);
+			trace.return_values.put(statement.getPosition(), retval);
+		}
+
+		// Add inspector calls on callee
+		if (statement instanceof MethodStatement) {
+			MethodStatement ms = (MethodStatement) statement;
+			if (!ms.isStatic()) {
+				inspectors = manager.getInspectors(ms.getMethod().getDeclaringClass());
+				if (!inspectors.isEmpty()) {
+					trace.calleeMap.put(statement.getPosition(),
+					                    new HashMap<Inspector, Object>());
+
+					VariableReference callee = ms.getCallee();
+					if (callee.getObject(scope) == null)
+						return;
+					for (Inspector i : inspectors) {
+						try {
+							Object value = i.getValue(callee.getObject(scope));
+							trace.calleeMap.get(statement.getPosition()).put(i, value);
+						} catch (Exception e) {
+							logger.info("Exception during call to inspector: " + e);
+						}
+					}
+				}
+			}
+		}
+
+	}
+
+	public InspectorTrace getTrace() {
+		return trace.clone();
+	}
+}
---
+++ src/main/java/de/unisb/cs/st/evosuite/testcase/StatementInterface.java
@@ -0,0 +1,200 @@
+/**
+ * 
+ */
+package de.unisb.cs.st.evosuite.testcase;
+
+import java.io.PrintStream;
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.objectweb.asm.commons.GeneratorAdapter;
+
+import de.unisb.cs.st.evosuite.assertion.Assertion;
+
+/**
+ * @author Sebastian Steenbuck
+ * 
+ */
+public interface StatementInterface {
+
+	/**
+	 * Check if the statement makes use of var
+	 * 
+	 * @param var
+	 *            Variable we are checking for
+	 * @return True if var is referenced
+	 */
+	public boolean references(VariableReference var);
+
+	/**
+	 * Replace a VariableReference with another one
+	 * 
+	 * @param var1
+	 *            The old variable
+	 * @param var2
+	 *            The new variable
+	 */
+	public void replace(VariableReference var1, VariableReference var2);
+
+	public Throwable execute(Scope scope, PrintStream out)
+	        throws InvocationTargetException, IllegalArgumentException,
+	        IllegalAccessException, InstantiationException;
+
+	/**
+	 * Get Java representation of statement
+	 * 
+	 * @return
+	 */
+	public String getCode();
+
+	/**
+	 * Various consistency checks. This method might also return with an
+	 * assertionError Functionality might depend on the status of
+	 * enableAssertions in this JVM
+	 * 
+	 * @return
+	 */
+	public boolean isValid();
+
+	/**
+	 * Get Java representation of statement
+	 * 
+	 * @return
+	 */
+	public String getCode(Throwable exception);
+
+	/**
+	 * Generate bytecode by calling method generator
+	 * 
+	 * @param mg
+	 */
+	public void getBytecode(GeneratorAdapter mg, Map<Integer, Integer> locals,
+	        Throwable exception);
+
+	/**
+	 * 
+	 * @return Generic type of return value
+	 */
+	public Type getReturnType();
+
+	/**
+	 * 
+	 * @return Raw class of return value
+	 */
+	public Class<?> getReturnClass();
+
+	/**
+	 * Equality check
+	 * 
+	 * @param s
+	 *            Other statement
+	 * @return True if equals
+	 */
+	@Override
+	public boolean equals(Object s);
+
+	/**
+	 * Generate hash code
+	 */
+	@Override
+	public int hashCode();
+
+	/**
+	 * @return Variable representing return value
+	 */
+	public VariableReference getReturnValue();
+
+	public Set<VariableReference> getVariableReferences();
+
+	public List<VariableReference> getUniqueVariableReferences();
+
+	/**
+	 * 
+	 * @param newTestCase
+	 *            the testcase in which this statement will be inserted
+	 * @return
+	 */
+	public StatementInterface clone(TestCase newTestCase);
+
+	/**
+	 * Create deep copy of statement
+	 */
+	public StatementInterface clone();
+
+	/**
+	 * Check if there are assertions
+	 * 
+	 * @return True if there are assertions
+	 */
+	public boolean hasAssertions();
+
+	/**
+	 * Add a new assertion to statement
+	 * 
+	 * @param assertion
+	 *            Assertion to be added
+	 */
+	public void addAssertion(Assertion assertion);
+
+	/**
+	 * Get Java code representation of assertions
+	 * 
+	 * @return String representing all assertions attached to this statement
+	 */
+	public String getAssertionCode();
+
+	/**
+	 * Delete all assertions attached to this statement
+	 */
+	public void removeAssertions();
+
+	/**
+	 * Delete assertion attached to this statement
+	 */
+	public void removeAssertion(Assertion assertion);
+
+	/**
+	 * Return list of assertions
+	 */
+	public Set<Assertion> getAssertions();
+
+	public Set<Class<?>> getDeclaredExceptions();
+
+	public int getPosition();
+
+	/**
+	 * Allows the comparing of Statements between TestCases. I.e. this is a more
+	 * semantic comparison than the one done by equals. E.g. two Variable are
+	 * equal if they are at the same position and they reference to objects of
+	 * the same type.
+	 * 
+	 * @param s
+	 * @return
+	 */
+	public boolean same(StatementInterface s);
+
+	public boolean isValidException(Throwable t);
+
+	public boolean mutate(TestCase test, AbstractTestFactory factory);
+
+	public void SetRetval(VariableReference newRetVal);
+	
+	/**
+	 * Returns the accessibleObject which is used to generate this kind of statement
+	 * E.g. the Field of a FieldStatement, the Method of a MethodStatement and so on
+	 * MAY return NULL (for example for NullStatements)
+	 * @return
+	 */
+	public AccessibleObject getAccessibleObject();
+
+	/**
+	 * Returns true if this statement should be handled as an AssignmentStatement. 
+	 * This method was added to allow the wrapping of AssignmentStatements (in which case "a instanceof AssignmentStatement" is no longer working)
+	 * @return
+	 */
+	public boolean isAssignmentStatement();
+}
