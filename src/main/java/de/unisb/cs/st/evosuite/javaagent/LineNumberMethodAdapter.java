34bd72c6f7e5eb6cb15928951937b9d5e6225734
lang <devnull@localhost>
2011-07-21 04:06:46 +0200

    Added properties write_excel: to choose, if excel output is wished. comp_lcsaj_branch: for my evaluation of the branch coverage criterion wenn applied to lcsaj fitnesses. Also some additions to Exceloutput and a slightly changed LCSAJ instrumentation analysis .

---
+++ src/main/java/de/unisb/cs/st/evosuite/javaagent/LineNumberMethodAdapter.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2010 Saarland University
+ * 
+ * This file is part of EvoSuite.
+ * 
+ * EvoSuite is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser Public License along with
+ * EvoSuite. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.unisb.cs.st.evosuite.javaagent;
+
+import org.apache.log4j.Logger;
+import org.objectweb.asm.Label;
+import org.objectweb.asm.MethodAdapter;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+/**
+ * Instruments classes to call the tracer each time a new line of the source
+ * code is passed.
+ * 
+ * @author Gordon Fraser
+ * 
+ */
+public class LineNumberMethodAdapter extends MethodAdapter {
+
+	public static int branch_id = 0;
+
+	@SuppressWarnings("unused")
+	private static Logger logger = Logger.getLogger(LineNumberMethodAdapter.class);
+
+	private final String fullMethodName;
+
+	private final String methodName;
+
+	private final String className;
+
+	int current_line = 0;
+
+	public LineNumberMethodAdapter(MethodVisitor mv, String className, String methodName,
+	        String desc) {
+		super(mv);
+		fullMethodName = methodName + desc;
+		this.className = className;
+		this.methodName = methodName;
+	}
+
+	@Override
+	public void visitLineNumber(int line, Label start) {
+		super.visitLineNumber(line, start);
+		if (methodName.equals("<clinit>"))
+			return;
+
+		this.visitLdcInsn(className);
+		this.visitLdcInsn(fullMethodName);
+		this.visitLdcInsn(line);
+		mv.visitMethodInsn(Opcodes.INVOKESTATIC,
+		                   "de/unisb/cs/st/evosuite/testcase/ExecutionTracer",
+		                   "passedLine", "(Ljava/lang/String;Ljava/lang/String;I)V");
+		current_line = line;
+	}
+}
