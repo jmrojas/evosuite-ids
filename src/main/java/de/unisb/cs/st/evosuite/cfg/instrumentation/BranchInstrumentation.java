9b1112d37077ec06d2d95ebf79b9a9b93523501f
Jeremias Roessler <roessler@cs.uni-saarland.de>
2011-07-26 14:07:05 +0200

    Adding the feature to read in (simple) existing test cases for initial seeding.

---
+++ src/main/java/de/unisb/cs/st/evosuite/cfg/instrumentation/BranchInstrumentation.java
@@ -0,0 +1,215 @@
+/**
+ * 
+ */
+package de.unisb.cs.st.evosuite.cfg.instrumentation;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.InsnNode;
+import org.objectweb.asm.tree.LdcInsnNode;
+import org.objectweb.asm.tree.LookupSwitchInsnNode;
+import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.TableSwitchInsnNode;
+
+import de.unisb.cs.st.evosuite.cfg.BytecodeInstruction;
+import de.unisb.cs.st.evosuite.cfg.CFGPool;
+import de.unisb.cs.st.evosuite.cfg.RawControlFlowGraph;
+import de.unisb.cs.st.evosuite.coverage.branch.BranchPool;
+
+/**
+ * @author Copied from CFGMethodAdapter
+ * 
+ */
+public class BranchInstrumentation implements MethodInstrumentation {
+
+	private static Logger logger = Logger.getLogger(BranchInstrumentation.class);
+
+	/* (non-Javadoc)
+	 * @see de.unisb.cs.st.evosuite.cfg.MethodInstrumentation#analyze(org.objectweb.asm.tree.MethodNode, org.jgrapht.Graph, java.lang.String, java.lang.String, int)
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public void analyze(MethodNode mn, String className, String methodName, int access) {
+		RawControlFlowGraph graph = CFGPool.getRawCFG(className, methodName);
+		Iterator<AbstractInsnNode> j = mn.instructions.iterator();
+		while (j.hasNext()) {
+			AbstractInsnNode in = j.next();
+			for (BytecodeInstruction v : graph.vertexSet()) {
+
+				// If this is in the CFG and it's a branch...
+				if (in.equals(v.getASMNode())) {
+					if (v.isBranch()) {
+						mn.instructions.insert(v.getASMNode().getPrevious(),
+						                       getInstrumentation(v));
+
+					} else if (v.isSwitch()) {
+						mn.instructions.insertBefore(v.getASMNode(),
+						                             getSwitchInstrumentation(v, mn, className,
+						                                                methodName));
+					}
+				}
+			}
+		}
+	}
+
+	private InsnList getSwitchInstrumentation(BytecodeInstruction v, MethodNode mn,
+	        String className, String methodName) {
+		InsnList instrumentation = new InsnList();
+
+		if(!v.isSwitch())
+			throw new IllegalArgumentException("switch instruction expected");
+		
+		Map<Integer,Integer> caseValuesToBranchIDs = BranchPool.getBranchIdMapForSwitch(v);
+		if (caseValuesToBranchIDs == null || caseValuesToBranchIDs.isEmpty())
+			throw new IllegalStateException(
+			        "expect BranchPool to know at least one branchID for each switch instruction");
+
+		String methodID = className + "." + methodName;
+
+		for (Integer targetCaseValue : caseValuesToBranchIDs.keySet()) {
+			instrumentation.add(new InsnNode(Opcodes.DUP));
+			instrumentation.add(new LdcInsnNode(targetCaseValue));
+			instrumentation.add(new LdcInsnNode(Opcodes.IF_ICMPEQ));
+			instrumentation.add(new LdcInsnNode(caseValuesToBranchIDs.get(targetCaseValue)));
+			instrumentation.add(new LdcInsnNode(v.getInstructionId()));
+			instrumentation.add(new MethodInsnNode(Opcodes.INVOKESTATIC,
+			        "de/unisb/cs/st/evosuite/testcase/ExecutionTracer",
+			        "passedBranch", "(IIIII)V"));
+			BranchPool.countBranch(methodID);
+		}
+
+		return instrumentation;
+	}
+
+	private InsnList getInstrumentation(BytecodeInstruction instruction) {
+		if (instruction == null)
+			throw new IllegalArgumentException("null given");
+		if (!instruction.isActualBranch())
+			throw new IllegalArgumentException("branch instruction expected");
+
+		int opcode = instruction.getASMNode().getOpcode();
+		int instructionId = instruction.getInstructionId();
+		String className = instruction.getClassName();
+		String methodName = instruction.getMethodName();
+
+		int branchId = BranchPool.getActualBranchIdForInstruction(instruction);
+		if (branchId < 0)
+			throw new IllegalStateException(
+			        "expect BranchPool to know branchId for alle branch instructions");
+
+		InsnList instrumentation = new InsnList();
+		String methodID = className + "." + methodName;
+
+		switch (opcode) {
+		case Opcodes.IFEQ:
+		case Opcodes.IFNE:
+		case Opcodes.IFLT:
+		case Opcodes.IFGE:
+		case Opcodes.IFGT:
+		case Opcodes.IFLE:
+			instrumentation.add(new InsnNode(Opcodes.DUP));
+			instrumentation.add(new LdcInsnNode(opcode));
+			// instrumentation.add(new LdcInsnNode(id));
+			instrumentation.add(new LdcInsnNode(branchId));
+			instrumentation.add(new LdcInsnNode(instructionId));
+			instrumentation.add(new MethodInsnNode(Opcodes.INVOKESTATIC,
+			        "de/unisb/cs/st/evosuite/testcase/ExecutionTracer", "passedBranch",
+			        "(IIII)V"));
+			BranchPool.countBranch(methodID);
+			logger.debug("Adding passedBranch val=?, opcode=" + opcode + ", branch="
+			        + branchId + ", bytecode_id=" + instructionId);
+
+			break;
+		case Opcodes.IF_ICMPEQ:
+		case Opcodes.IF_ICMPNE:
+		case Opcodes.IF_ICMPLT:
+		case Opcodes.IF_ICMPGE:
+		case Opcodes.IF_ICMPGT:
+		case Opcodes.IF_ICMPLE:
+			instrumentation.add(new InsnNode(Opcodes.DUP2));
+			instrumentation.add(new LdcInsnNode(opcode));
+			// instrumentation.add(new LdcInsnNode(id));
+			instrumentation.add(new LdcInsnNode(branchId));
+			instrumentation.add(new LdcInsnNode(instructionId));
+			instrumentation.add(new MethodInsnNode(Opcodes.INVOKESTATIC,
+			        "de/unisb/cs/st/evosuite/testcase/ExecutionTracer", "passedBranch",
+			        "(IIIII)V"));
+			BranchPool.countBranch(methodID);
+
+			break;
+		case Opcodes.IF_ACMPEQ:
+		case Opcodes.IF_ACMPNE:
+			instrumentation.add(new InsnNode(Opcodes.DUP2));
+			instrumentation.add(new LdcInsnNode(opcode));
+			// instrumentation.add(new LdcInsnNode(id));
+			instrumentation.add(new LdcInsnNode(branchId));
+			instrumentation.add(new LdcInsnNode(instructionId));
+			instrumentation.add(new MethodInsnNode(Opcodes.INVOKESTATIC,
+			        "de/unisb/cs/st/evosuite/testcase/ExecutionTracer", "passedBranch",
+			        "(Ljava/lang/Object;Ljava/lang/Object;III)V"));
+			BranchPool.countBranch(methodID);
+			break;
+		case Opcodes.IFNULL:
+		case Opcodes.IFNONNULL:
+			instrumentation.add(new InsnNode(Opcodes.DUP));
+			instrumentation.add(new LdcInsnNode(opcode));
+			// instrumentation.add(new LdcInsnNode(id));
+			instrumentation.add(new LdcInsnNode(branchId));
+			instrumentation.add(new LdcInsnNode(instructionId));
+			instrumentation.add(new MethodInsnNode(Opcodes.INVOKESTATIC,
+			        "de/unisb/cs/st/evosuite/testcase/ExecutionTracer", "passedBranch",
+			        "(Ljava/lang/Object;III)V"));
+			BranchPool.countBranch(methodID);
+			break;
+
+		/*
+		case Opcodes.TABLESWITCH:
+		instrumentation.add(new InsnNode(Opcodes.DUP));
+		instrumentation.add(new LdcInsnNode(opcode));
+		// instrumentation.add(new LdcInsnNode(id));
+		instrumentation.add(new LdcInsnNode(BranchPool.getBranchCounter()));
+		instrumentation.add(new LdcInsnNode(id));
+		instrumentation.add(new MethodInsnNode(Opcodes.INVOKESTATIC,
+		    "de/unisb/cs/st/evosuite/testcase/ExecutionTracer", "passedBranch",
+		    "(IIII)V"));
+		BranchPool.countBranch(methodID);
+		break;
+		case Opcodes.LOOKUPSWITCH:
+		instrumentation.add(new InsnNode(Opcodes.DUP));
+		instrumentation.add(new LdcInsnNode(opcode));
+		// instrumentation.add(new LdcInsnNode(id));
+		instrumentation.add(new LdcInsnNode(BranchPool.getBranchCounter()));
+		instrumentation.add(new LdcInsnNode(id));
+		instrumentation.add(new MethodInsnNode(Opcodes.INVOKESTATIC,
+		    "de/unisb/cs/st/evosuite/testcase/ExecutionTracer", "passedBranch",
+		    "(IIII)V"));
+		BranchPool.countBranch(methodID);
+		break;
+		 */
+		}
+		return instrumentation;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.unisb.cs.st.evosuite.cfg.MethodInstrumentation#executeOnExcludedMethods()
+	 */
+	@Override
+	public boolean executeOnExcludedMethods() {
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.unisb.cs.st.evosuite.cfg.MethodInstrumentation#executeOnMainMethod()
+	 */
+	@Override
+	public boolean executeOnMainMethod() {
+		return false;
+	}
+
+}
---
+++ src/main/java/de/unisb/cs/st/evosuite/ga/stoppingconditions/StoppingCondition.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright (C) 2010 Saarland University
+ * 
+ * This file is part of the GA library.
+ * 
+ * GA is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * GA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser Public License along with
+ * GA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.unisb.cs.st.evosuite.ga.stoppingconditions;
+
+import java.text.NumberFormat;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+
+import de.unisb.cs.st.evosuite.ga.Chromosome;
+import de.unisb.cs.st.evosuite.ga.GeneticAlgorithm;
+import de.unisb.cs.st.evosuite.ga.SearchListener;
+
+/**
+ * Base class of decision functions that stop the search
+ * 
+ * @author Gordon Fraser
+ * 
+ */
+public abstract class StoppingCondition implements SearchListener {
+
+	protected static Logger logger = Logger.getLogger(StoppingCondition.class);
+
+	public StoppingCondition() {
+		reset();
+	}
+
+	public abstract boolean isFinished();
+
+	@Override
+	public void searchStarted(GeneticAlgorithm algorithm) {
+
+	}
+
+	@Override
+	public void fitnessEvaluation(Chromosome chromosome) {
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see de.unisb.cs.st.ga.SearchListener#iteration(java.util.List)
+	 */
+	@Override
+	public void iteration(GeneticAlgorithm algorithm) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see de.unisb.cs.st.ga.SearchListener#searchFinished(java.util.List)
+	 */
+	@Override
+	public void searchFinished(GeneticAlgorithm algorithm) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * de.unisb.cs.st.evosuite.ga.SearchListener#mutation(de.unisb.cs.st.evosuite
+	 * .ga.Chromosome)
+	 */
+	@Override
+	public void modification(Chromosome individual) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Reset everything
+	 */
+	public abstract void reset();
+
+	/**
+	 * Set new upper limit of resources
+	 * 
+	 * @param limit
+	 */
+	public abstract void setLimit(int limit);
+
+	/**
+	 * Get upper limit of resources
+	 * 
+	 * Mainly used for toString()
+	 * 
+	 * @return limit
+	 */
+	public abstract int getLimit();
+
+	/**
+	 * How much of the budget have we used up
+	 * 
+	 * @return
+	 */
+	public abstract int getCurrentValue();
+
+	@Override
+	public String toString() {
+		StringBuilder r = new StringBuilder();
+		String type = getClass().toString();
+		try { // just to make sure
+			type = type.substring(type.lastIndexOf(".") + 1);
+		} catch (Exception e) {
+		}
+		type = type.substring(0, type.length() - 17); // cut away "StoppingCondition" suffix
+		type += " :";
+		type = StringUtils.rightPad(type, 24);
+		r.append(type);
+		String value = NumberFormat.getIntegerInstance().format(getCurrentValue());
+		value = StringUtils.leftPad(value, 12);
+		String limit = NumberFormat.getIntegerInstance().format(getLimit());
+		limit = StringUtils.rightPad(limit, 12);
+		r.append(value + " / " + limit);
+		if (isFinished())
+			r.append(" Finished!");
+
+		return r.toString();
+	}
+}
