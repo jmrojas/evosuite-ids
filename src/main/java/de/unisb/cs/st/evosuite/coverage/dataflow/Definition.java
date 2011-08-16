570b34acbcb0165890f2ec0f7df3da8207f1901e
mis <mis@localhost>
2011-08-16 17:38:24 +0200

    getting alternative defuse fitness calculation to work properly - another unsuccessful try

---
+++ src/main/java/de/unisb/cs/st/evosuite/coverage/dataflow/Definition.java
@@ -0,0 +1,42 @@
+package de.unisb.cs.st.evosuite.coverage.dataflow;
+
+import de.unisb.cs.st.evosuite.cfg.BytecodeInstruction;
+
+/**
+ * An object of this class corresponds to a Definition inside the class under test.
+ * 
+ * Definitions are created by the DefUseFactory via the DefUsePool.
+ * 
+ * @author Andre Mis
+ */
+
+public class Definition extends DefUse {
+
+	Definition(BytecodeInstruction wrap, int defuseId, int defId, int useId,
+			boolean isParameterUse) {
+	
+		super(wrap, defuseId, defId, useId, isParameterUse);
+		if (!isDefinition())
+			throw new IllegalArgumentException(
+					"Vertex of a definition expected");
+	}
+	
+	/**
+	 * Determines whether this Definition ca be an active definition
+	 * for the given instruction.
+	 * 
+	 *  This is the case if instruction constitutes a Use for the 
+	 *  same variable as this Definition
+	 *  
+	 *  Not to be confused with DefUse.canBecomeActiveDefinitionFor,
+	 *  which is sort of the dual to this method
+	 */
+	public boolean canBeActiveFor(BytecodeInstruction instruction) {
+		if(!instruction.isUse())
+			return false;
+		
+		Use use = DefUseFactory.makeUse(instruction);
+		return sharesVariableWith(use);
+	}
+	
+}
---
+++ src/main/java/de/unisb/cs/st/evosuite/javaagent/InstrumentingClassLoader.java
@@ -0,0 +1,113 @@
+package de.unisb.cs.st.evosuite.javaagent;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.objectweb.asm.ClassReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.unisb.cs.st.evosuite.Properties;
+
+public class InstrumentingClassLoader extends ClassLoader {
+	private final static Logger logger = LoggerFactory.getLogger(InstrumentingClassLoader.class);
+	private final BytecodeInstrumentation instrumentation;
+	private final ClassLoader classLoader;
+	private final Map<String, Class<?>> classes = new HashMap<String, Class<?>>();
+
+	public InstrumentingClassLoader() {
+		this(new BytecodeInstrumentation());
+	}
+
+	public InstrumentingClassLoader(BytecodeInstrumentation instrumentation) {
+		super(InstrumentingClassLoader.class.getClassLoader());
+		classLoader = InstrumentingClassLoader.class.getClassLoader();
+		this.instrumentation = instrumentation;
+	}
+
+	@Override
+	public Class<?> loadClass(String name) throws ClassNotFoundException {
+		if (instrumentation.isTargetProject(name)) {
+			// if (TestCluster.isTargetClassName(name)) {
+			Class<?> result = findLoadedClass(name);
+			if (result != null) {
+				return result;
+			} else {
+				result = classes.get(name);
+				if (result != null) {
+					return result;
+				} else {
+					logger.info("Seeing class for first time: " + name);
+					return instrumentClass(name);
+				}
+			}
+		} else {
+			logger.trace("Not instrumenting: " + name);
+		}
+		Class<?> result = findLoadedClass(name);
+		if (result != null) {
+			return result;
+		}
+		result = classLoader.loadClass(name);
+		return result;
+	}
+
+	private Class<?> instrumentClass(String fullyQualifiedTargetClass)
+	        throws ClassNotFoundException {
+		logger.info("Instrumenting class '" + fullyQualifiedTargetClass + "'.");
+		try {
+			String className = fullyQualifiedTargetClass.replace('.', '/');
+			InputStream is = ClassLoader.getSystemResourceAsStream(className + ".class");
+			if (is == null) {
+				throw new ClassNotFoundException(
+				        "Class should be in target project, but could not be found!");
+			}
+			byte[] byteBuffer = instrumentation.transformBytes(className,
+			                                                   new ClassReader(is));
+			Class<?> result = defineClass(fullyQualifiedTargetClass, byteBuffer, 0,
+			                              byteBuffer.length);
+			classes.put(fullyQualifiedTargetClass, result);
+			logger.info("Keeping class: " + fullyQualifiedTargetClass);
+			return result;
+		} catch (Exception e) {
+			throw new ClassNotFoundException(e.getMessage(),e);
+		}
+	}
+
+	private boolean isTargetClass(String className) {
+		if (className.equals(Properties.TARGET_CLASS)
+		        || className.startsWith(Properties.TARGET_CLASS + "$")) {
+			return true;
+		}
+		return false;
+	}
+
+	private Class<?> loadClassByteCode(String name) throws ClassNotFoundException {
+		if (name.startsWith("java.") || name.startsWith("sun.")) {
+			throw new IllegalStateException("Cannot load java system class: " + name);
+		}
+		try {
+			InputStream is = ClassLoader.getSystemResourceAsStream(name.replace('.', '/')
+			        + ".class");
+			if (is == null) {
+				throw new ClassNotFoundException(
+				        "Class should be in target project, but could not be found!");
+			}
+			ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+			int nRead;
+			byte[] data = new byte[16384];
+			while ((nRead = is.read(data, 0, data.length)) != -1) {
+				buffer.write(data, 0, nRead);
+			}
+			buffer.flush();
+			byte[] byteBuffer = buffer.toByteArray();
+			Class<?> result = defineClass(name, byteBuffer, 0, byteBuffer.length);
+			return result;
+		} catch (IOException exc) {
+			return null;
+		}
+	}
+}
