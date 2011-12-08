bb5173f99806e4273318243b25812405ef452084
fraser <fraser@localhost>
2011-12-08 17:09:05 +0100

    Add methods to access list of available classes

---
+++ src/main/java/de/unisb/cs/st/evosuite/contracts/HashCodeReturnsNormallyContract.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package de.unisb.cs.st.evosuite.contracts;
+
+import de.unisb.cs.st.evosuite.testcase.Scope;
+import de.unisb.cs.st.evosuite.testcase.StatementInterface;
+import de.unisb.cs.st.evosuite.testcase.TestCaseExecutor.TimeoutExceeded;
+
+/**
+ * @author Gordon Fraser
+ * 
+ */
+public class HashCodeReturnsNormallyContract extends Contract {
+
+	/* (non-Javadoc)
+	 * @see de.unisb.cs.st.evosuite.contracts.Contract#check(de.unisb.cs.st.evosuite.testcase.Statement, de.unisb.cs.st.evosuite.testcase.Scope, java.lang.Throwable)
+	 */
+	@Override
+	public boolean check(StatementInterface statement, Scope scope, Throwable exception) {
+		for (Object object : getAllObjects(scope)) {
+			if (object == null)
+				continue;
+
+			try {
+				// hashCode must not throw an exception
+				object.hashCode();
+
+			} catch (Throwable t) {
+				if (!(t instanceof TimeoutExceeded))
+					return false;
+			}
+		}
+
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "hashCode returns normally check";
+	}
+}
