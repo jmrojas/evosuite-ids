032d60a0bba4b45483b1a66fa698e4e05c610ca4
rusev <devnull@localhost>
2012-01-08 09:04:33 +0100

    Fixed some warnings

---
+++ src/main/java/de/unisb/cs/st/evosuite/symbolic/expr/StringMultipleExpression.java
@@ -0,0 +1,146 @@
+/**
+ * 
+ */
+package de.unisb.cs.st.evosuite.symbolic.expr;
+
+import java.util.ArrayList;
+
+/**
+ * @author krusev
+ *
+ */
+public class StringMultipleExpression extends StringBinaryExpression implements
+BinaryExpression<String>{
+
+	private static final long serialVersionUID = 7172041118401792672L;
+
+	protected ArrayList<Expression<?>> other_v;
+
+	public StringMultipleExpression(Expression<String> _left, Operator _op,
+	        Expression<?> _right, ArrayList<Expression<?>> _other, String con) {
+		super(_left, _op, _right, con);
+		this.other_v = _other;
+	}
+
+	/**
+	 * @return the other
+	 */
+	public ArrayList<Expression<?>> getOther() {
+		return other_v;
+	}
+
+	@Override
+	public String getConcreteValue() {
+		return super.getConcreteValue();
+	}
+
+	@Override
+	public Operator getOperator() {
+		return super.getOperator();
+	}
+
+	@Override
+	public Expression<String> getLeftOperand() {
+		return super.getLeftOperand();
+	}
+
+	@Override
+	public Expression<?> getRightOperand() {
+		return super.getRightOperand();
+	}
+
+	@Override
+	public String toString() {
+		String str_other_v = "";
+		for (int i = 0; i < this.other_v.size(); i++) {
+			str_other_v += " " + this.other_v.get(i).toString();
+		}
+		
+		return "(" + left + op.toString() + (right==null ? "": right) + str_other_v + ")";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this) {
+			return true;
+		}
+		if (obj instanceof StringMultipleExpression) {
+			StringMultipleExpression other = (StringMultipleExpression) obj;
+			
+			boolean other_v_eq = true;
+			
+			if (other.other_v.size() == this.other_v.size()) {
+				for (int i = 0; i < other.other_v.size(); i++) {
+					if ( !( other.other_v.get(i).equals(this.other_v.get(i)) ) ) {
+						other_v_eq = false;
+					}
+				}
+			} else {
+				other_v_eq = false;
+			}
+			
+			return this.op.equals(other.op) && this.getSize() == other.getSize()
+			        && this.left.equals(other.left) && this.right.equals(other.right)
+			        && other_v_eq;
+		}
+
+		return false;
+	}
+
+	protected int size = 0;
+
+	@Override
+	public int getSize() {
+		//TODO fix this
+		return -1;
+//		if (size == 0) {
+//			size = 1 + getLeftOperand().getSize() + getRightOperand().getSize();
+//		}
+//		return size;
+	}
+	
+	@Override
+	public String execute() {
+		String first = (String)left.execute();
+		Object second = right.execute();		
+		Object third = other_v.get(0).execute();
+		long secLong, thrdLong;
+		String secStr, thrdStr;
+		
+		switch (op) {
+		
+		case INDEXOFCI:
+			secLong = (Long) second;
+			thrdLong = (Long) third;
+			return Integer.toString(first.indexOf((int)secLong, (int)thrdLong));
+		case INDEXOFSI:
+			secStr = (String) second;
+			thrdLong = (Long) third;
+			return Integer.toString(first.indexOf(secStr, (int)thrdLong));
+		case SUBSTRING:
+			secLong = (Long) second;
+			thrdLong = (Long) third;
+			return first.substring((int) secLong, (int) thrdLong);
+		case REPLACEC:
+			secLong = (Long) second;
+			thrdLong = (Long) third;
+			return first.replace((char) secLong, (char) thrdLong);
+		case REPLACECS:
+			secStr = (String) second;
+			thrdStr = (String) third;
+			return first.replace(secStr, thrdStr);
+		case REPLACEALL:
+			secStr = (String) second;
+			thrdStr = (String) third;
+			return first.replaceAll(secStr, thrdStr);
+		case REPLACEFIRST:				
+			secStr = (String) second;
+			thrdStr = (String) third;
+			return first.replaceFirst(secStr, thrdStr);
+		default:
+			log.warning("StringBinaryExpression: unimplemented operator!");
+			return null;
+		}
+	}
+
+}
---
+++ src/main/java/de/unisb/cs/st/evosuite/symbolic/search/Seeker.java
@@ -0,0 +1,280 @@
+/**
+ * 
+ */
+package de.unisb.cs.st.evosuite.symbolic.search;
+
+import gov.nasa.jpf.JPF;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Logger;
+
+import de.unisb.cs.st.evosuite.symbolic.Solver;
+import de.unisb.cs.st.evosuite.symbolic.expr.BinaryExpression;
+import de.unisb.cs.st.evosuite.symbolic.expr.Constraint;
+import de.unisb.cs.st.evosuite.symbolic.expr.Expression;
+
+import de.unisb.cs.st.evosuite.symbolic.expr.IntegerVariable;
+import de.unisb.cs.st.evosuite.symbolic.expr.RealVariable;
+import de.unisb.cs.st.evosuite.symbolic.expr.StringComparison;
+import de.unisb.cs.st.evosuite.symbolic.expr.StringMultipleComparison;
+import de.unisb.cs.st.evosuite.symbolic.expr.StringVariable;
+import de.unisb.cs.st.evosuite.symbolic.expr.UnaryExpression;
+import de.unisb.cs.st.evosuite.symbolic.expr.Variable;
+
+/**
+ * @author krusev
+ *
+ */
+public class Seeker implements Solver {
+	
+	static Logger log = JPF.getLogger("de.unisb.cs.st.evosuite.symbolic.search.Seeker");
+	
+	//TODO figure out what values should be given to these fields
+	private int maxStepsForAll = 3;
+	
+	
+	/* The idea here is to get the expressions and build the constraint 
+	 * dynamically here using Java reflection. This should save us some time
+	 * since we wan't do the evaluation of the constraints in JPF 
+	 * 
+	 * In getModel we need to build the constraints and search for better input 
+	 * values for the String variables. 
+	 */
+	public Map<String, Object> getModel(Collection<Constraint<?>> constraints){
+		HashMap<String, Object> result = new HashMap<String, Object>();
+	
+		//This actually does get a list every time the super class Solver is 
+		// implemented using a collection so if we are going to throw away 
+		// the other Solvers we might as well change this
+		List<Constraint<?>> cnstr = null;
+		if (constraints instanceof List<?>) {
+			cnstr = (List<Constraint<?>>)constraints;
+		} else {
+			log.warning("Seeker got other type of collections!");
+			return null;
+		}
+		
+		//Get the target cnstr and the variables in it
+		Constraint<?> target = cnstr.get(cnstr.size()-1);
+		Set<Variable<?>> vars = getVariables(target);
+		//remove the target from the constraints
+		cnstr.remove(target);
+		
+		outerloop:
+		for (int i = 0; i < maxStepsForAll ; i++ ) {
+			for (Variable<?> var : vars) {
+				
+				Changer changer = new Changer();
+				
+				if (var instanceof StringVariable) {
+					StringVariable strVar = (StringVariable) var;
+					if (changer.strLocalSearch(strVar, target, cnstr, result)) {
+						break outerloop;
+					}
+				}
+				// These two are not yet implemented
+				if (var instanceof IntegerVariable) {
+					IntegerVariable intVar = (IntegerVariable) var;
+					if (changer.intLocalSearch(intVar, target, cnstr, result)) {
+						break outerloop;
+					}
+				}
+				if (var instanceof RealVariable) {
+					RealVariable realVar = (RealVariable) var;
+					if (changer.realLocalSearch(realVar, target, cnstr, result)) {
+						break outerloop;
+					}
+				}				
+			}
+		}
+		
+		
+		return result;
+	}
+	
+	
+	
+
+	
+	/*
+	//Old getModel. Please remove me!
+	public Map<String, Object> getModel(Collection<Constraint<?>> constraints){
+		HashMap<String, Object> result = new HashMap<String, Object>();
+
+		
+		List<Constraint<?>> cnstr = (List<Constraint<?>>)constraints;
+		Constraint<?> target = cnstr.get(cnstr.size()-1);
+		
+		//remove the target from the constraints
+		constraints.remove(target);
+		
+		boolean desCnstrValue; 
+		switch (target.getComparator()) {
+		case EQ:	//TODO what to do if we don't want to satisfy the condition? Just exit?
+			desCnstrValue = false;
+			break;
+		case NE:	//this means we want to satisfy the condition
+			desCnstrValue = true;
+			break;
+		case GE:	// We should't get any of this for Strings 
+		case GT:	//TODO also handle integers
+		case LE:
+		case LT:
+		default:
+			throw new RuntimeException("Unsupported comparison: " + target.getComparator());
+		}
+		
+		Expression<?> expr = target.getLeftOperand();
+		
+		if (!(expr instanceof StringComparison)) {
+			return null;
+		}
+		StringComparison sc = (StringComparison) expr;
+		
+		// We don't need this functionality for now
+		// If we don't need it at all delete "parent" stuff in Expression
+
+		//setupTree(sc);
+		
+		Set<StringVariable> vars = getStringVariables(target);
+		
+		int fitness;// = DistanceEstimator.getFitness(sc);
+		boolean reachable = true;
+		
+		for (int i = 0; i < maxStepsForAll ; i++ ) {
+			for (Variable<?> var : vars) {
+				fitness = DistanceEstimator.getFitness(sc);
+				Changer changer = new Changer();
+				String strVal = var.getMaxValue();
+				
+				for (int j = 0; j < maxStepsForEach ; j++ ) {
+					String newVal = changer.changeVar(strVal, fitness, reachable);
+					//assign working value in var.MinValue)
+					var.setMinValue(newVal);
+					reachable = DistanceEstimator.areReachable(constraints);
+					//if unreachable values will be reverted automatically by changeVar()
+					if (reachable) {
+						int newFit = DistanceEstimator.getFitness(sc);
+						if (newFit > fitness) {
+							var.setMaxValue(newVal);
+							result.put(var.getName() , newVal);
+						}	
+						if (newFit >= 0) {
+							//we are ready 
+							log.warning("we got: " + result);
+							return result;
+						} else {							
+							fitness = newFit;
+							strVal = newVal;
+						}
+					}
+				}
+			}
+		}
+	 	*/
+	
+	
+	@SuppressWarnings("unused")
+	private void setupTree(Expression<?> expr) {
+		if (expr instanceof Variable<?>) {
+			//done
+		} else if (expr instanceof StringMultipleComparison){
+			StringMultipleComparison smc = (StringMultipleComparison) expr;
+			smc.getLeftOperand().setParent(expr);
+			setupTree(smc.getLeftOperand());
+			smc.getRightOperand().setParent(expr);
+			setupTree(smc.getRightOperand());
+			
+			ArrayList<Expression<?>> ar_l_ex = smc.getOther();
+			Iterator<Expression<?>> itr = ar_l_ex.iterator();
+		    while (itr.hasNext()) {
+		    	Expression<?> element = itr.next();
+		    	element.setParent(expr);
+		    	setupTree(element);
+		    }
+		} else if (expr instanceof StringComparison){
+			StringComparison sc = (StringComparison) expr;
+			sc.getLeftOperand().setParent(expr);
+			setupTree(sc.getLeftOperand());
+			sc.getRightOperand().setParent(expr);
+			setupTree(sc.getRightOperand());
+			
+		} else if (expr instanceof BinaryExpression<?>) {
+			BinaryExpression<?> bin = (BinaryExpression<?>) expr;
+			bin.getLeftOperand().setParent(expr);
+			setupTree(bin.getLeftOperand());
+			bin.getRightOperand().setParent(expr);
+			setupTree(bin.getRightOperand());
+
+		} else if (expr instanceof UnaryExpression<?>) {
+			UnaryExpression<?> un = (UnaryExpression<?>) expr;
+			un.getOperand().setParent(expr);
+			setupTree(un.getOperand());
+			
+		} else if (expr instanceof Constraint<?>) {
+			// ignore
+
+		}		
+	}
+
+	public boolean solve(Collection<Constraint<?>> constraints){
+		return false;
+	}
+
+	/**
+	 * Determine the set of variable referenced by this constraint
+	 * 
+	 * @param constraint
+	 * @return
+	 */
+	private Set<Variable<?>> getVariables(Constraint<?> constraint) {
+		Set<Variable<?>> variables = new HashSet<Variable<?>>();
+		getVariables(constraint.getLeftOperand(), variables);
+		getVariables(constraint.getRightOperand(), variables);
+		return variables;
+	}
+
+	/**
+	 * Recursively determine constraints in expression
+	 * 
+	 * @param expr
+	 * @param variables
+	 */
+	private void getVariables(Expression<?> expr, Set<Variable<?>> variables) {
+		if (expr instanceof Variable<?>) {
+			variables.add((Variable<?>) expr);
+		} else if (expr instanceof StringMultipleComparison){
+			StringMultipleComparison smc = (StringMultipleComparison) expr;
+			getVariables(smc.getLeftOperand(), variables);
+			getVariables(smc.getRightOperand(), variables);
+			ArrayList<Expression<?>> ar_l_ex = smc.getOther();
+			Iterator<Expression<?>> itr = ar_l_ex.iterator();
+		    while (itr.hasNext()) {
+		    	Expression<?> element = itr.next();
+		    	getVariables(element, variables);
+		    }
+		} else if (expr instanceof StringComparison){
+			StringComparison sc = (StringComparison) expr;
+			getVariables(sc.getLeftOperand(), variables);
+			getVariables(sc.getRightOperand(), variables);
+		} else if (expr instanceof BinaryExpression<?>) {
+			BinaryExpression<?> bin = (BinaryExpression<?>) expr;
+			getVariables(bin.getLeftOperand(), variables);
+			getVariables(bin.getRightOperand(), variables);
+		} else if (expr instanceof UnaryExpression<?>) {
+			UnaryExpression<?> un = (UnaryExpression<?>) expr;
+			getVariables(un.getOperand(), variables);
+		} else if (expr instanceof Constraint<?>) {
+			// ignore
+
+		}
+	}
+	
+}
