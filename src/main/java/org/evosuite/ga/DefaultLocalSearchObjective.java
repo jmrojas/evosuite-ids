6f7ef013007d3ccaf52ae7c2a2494925e3cb9731
may <may@st.cs.uni-saarland.de>
2012-07-04 11:16:43 +0200

    Code duplication in DefaultLocalSearchObjective removed. Also coverage values should now be reset correctly in every case.

---
+++ src/main/java/org/evosuite/ga/DefaultLocalSearchObjective.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (C) 2011,2012 Gordon Fraser, Andrea Arcuri and EvoSuite
+ * contributors
+ *
+ * This file is part of EvoSuite.
+ *
+ * EvoSuite is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Public License for more details.
+ *
+ * You should have received a copy of the GNU Public License along with
+ * EvoSuite. If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * 
+ */
+package org.evosuite.ga;
+
+import java.io.Serializable;
+
+/**
+ * @author Gordon Fraser
+ * 
+ */
+public class DefaultLocalSearchObjective implements LocalSearchObjective, Serializable {
+
+	private static final long serialVersionUID = -8640106627078837108L;
+
+	private final FitnessFunction fitness;
+
+	public DefaultLocalSearchObjective(FitnessFunction fitness) {
+		this.fitness = fitness;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.evosuite.ga.LocalSearchObjective#hasImproved(org.evosuite.ga.Chromosome)
+	 */
+	@Override
+	public boolean hasImproved(Chromosome individual) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.evosuite.ga.LocalSearchObjective#getFitnessFunction()
+	 */
+	@Override
+	public FitnessFunction getFitnessFunction() {
+		return fitness;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.evosuite.ga.LocalSearchObjective#hasChanged(org.evosuite.ga.Chromosome)
+	 */
+	@Override
+	public int hasChanged(Chromosome individual) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.evosuite.ga.LocalSearchObjective#hasNotWorsened(org.evosuite.ga.Chromosome)
+	 */
+	@Override
+	public boolean hasNotWorsened(Chromosome individual) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
