19dea364e5fc7134c5d26bf4c6df082c5aea71dc
Juan Galeotti <galeotti@cs.uni-saarland.de>
2012-08-26 00:34:28 +0200

    bugfix: avoid DSE VM from crashing when casting null value to a primitive value type

---
+++ src/test/java/org/evosuite/symbolic/TestCase4.java
@@ -0,0 +1,15 @@
+package org.evosuite.symbolic;
+
+public class TestCase4 {
+
+	/**
+	 * @param args
+	 */
+	public static void test(long long0, int int0) {
+		LinkedList linkedList0 = new LinkedList();
+		linkedList0.add(long0);
+		Object object0 = linkedList0.get(int0);
+
+	}
+
+}
