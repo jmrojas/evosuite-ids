c30bf9101d49eb3105e204086107a608ead2be16
gordon <gordon@localhost>
2012-08-27 10:41:11 +0100

    Local search fixes

---
+++ src/test/java/org/evosuite/symbolic/TestCase3.java
@@ -0,0 +1,13 @@
+package org.evosuite.symbolic;
+
+public class TestCase3 {
+
+	/**
+	 * @param args
+	 */
+	public static void test(int int0) {
+		LinkedList linkedList0 = new LinkedList();
+		Object object0 = linkedList0.get(int0);
+	}
+
+}
