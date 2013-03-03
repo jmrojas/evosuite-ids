38448535c33456dc0580a823ff1caa6e68d389c8
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2013-03-03 07:09:16 +0000

    Add JSRInlinerAdapter so we can use COMPUTE_FRAMES for Java7

---
+++ src/main/java/org/evosuite/symbolic/vm/string/CharAt.java
@@ -0,0 +1,45 @@
+package org.evosuite.symbolic.vm.string;
+
+import org.evosuite.symbolic.expr.Operator;
+import org.evosuite.symbolic.expr.bv.IntegerValue;
+import org.evosuite.symbolic.expr.bv.StringBinaryToIntegerExpression;
+import org.evosuite.symbolic.expr.str.StringValue;
+import org.evosuite.symbolic.vm.NonNullReference;
+import org.evosuite.symbolic.vm.SymbolicEnvironment;
+import org.evosuite.symbolic.vm.SymbolicFunction;
+import org.evosuite.symbolic.vm.SymbolicHeap;
+
+public final class CharAt extends SymbolicFunction {
+
+	private static final String CHAR_AT = "charAt";
+
+	public CharAt(SymbolicEnvironment env) {
+		super(env, Types.JAVA_LANG_STRING, CHAR_AT,
+				Types.INT_TO_CHAR_DESCRIPTOR);
+	}
+
+	@Override
+	public Object executeFunction() {
+
+		String conc_str = (String) this.getConcReceiver();
+		NonNullReference symb_str = this.getSymbReceiver();
+		StringValue string_expr = env.heap.getField(Types.JAVA_LANG_STRING,
+				SymbolicHeap.$STRING_VALUE, conc_str, symb_str, conc_str);
+
+		IntegerValue index_expr = this.getSymbIntegerArgument(0);
+		char res = this.getConcCharRetVal();
+
+
+		if (string_expr.containsSymbolicVariable()
+				|| index_expr.containsSymbolicVariable()) {
+
+			StringBinaryToIntegerExpression strBExpr = new StringBinaryToIntegerExpression(
+					string_expr, Operator.CHARAT, index_expr, (long) res);
+
+			return strBExpr;
+
+		} else {
+			return this.getSymbIntegerRetVal();
+		}
+	}
+}
