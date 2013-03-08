973b7052dc8a875418d1e6706ceccc9fbede4182
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2013-03-08 07:59:16 +0000

    Updated TODOs for ibranch fitness

---
+++ src/main/java/org/evosuite/coverage/ibranch/IBranchTestFitness.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package org.evosuite.coverage.ibranch;
+
+import org.evosuite.coverage.branch.Branch;
+import org.evosuite.setup.CallContext;
+import org.evosuite.testcase.ExecutionResult;
+import org.evosuite.testcase.TestChromosome;
+import org.evosuite.testcase.TestFitnessFunction;
+
+/**
+ * @author Gordon Fraser
+ * 
+ */
+public class IBranchTestFitness extends TestFitnessFunction {
+
+	private static final long serialVersionUID = -1399396770125054561L;
+
+	private final Branch branch;
+
+	private final CallContext context;
+
+	public IBranchTestFitness(Branch branch, CallContext context) {
+		this.branch = branch;
+		this.context = context;
+	}
+
+	public Branch getBranch() {
+		return branch;
+	}
+
+	public CallContext getContext() {
+		return context;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.evosuite.testcase.TestFitnessFunction#getFitness(org.evosuite.testcase.TestChromosome, org.evosuite.testcase.ExecutionResult)
+	 */
+	@Override
+	public double getFitness(TestChromosome individual, ExecutionResult result) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.evosuite.testcase.TestFitnessFunction#compareTo(org.evosuite.testcase.TestFitnessFunction)
+	 */
+	@Override
+	public int compareTo(TestFitnessFunction other) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.evosuite.testcase.TestFitnessFunction#getTargetClass()
+	 */
+	@Override
+	public String getTargetClass() {
+		return branch.getClassName();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.evosuite.testcase.TestFitnessFunction#getTargetMethod()
+	 */
+	@Override
+	public String getTargetMethod() {
+		return branch.getMethodName();
+	}
+	
+	@Override
+	public String toString() {		
+		return "Branch "+branch.toString()+" in context: "+context.toString();
+	}
+
+}
---
+++ src/test/java/org/evosuite/TestTrivialForDynamicSeeding.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (C) 2011,2012 Gordon Fraser, Andrea Arcuri and EvoSuite
+ * contributors
+ * 
+ * This file is part of EvoSuite.
+ * 
+ * EvoSuite is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ * 
+ * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Public License for more details.
+ * 
+ * You should have received a copy of the GNU Public License along with
+ * EvoSuite. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.evosuite;
+
+import org.evosuite.ga.GeneticAlgorithm;
+import org.evosuite.primitives.ConstantPoolManager;
+import org.evosuite.testsuite.TestSuiteChromosome;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.examples.with.different.packagename.TrivialForDynamicSeeding;
+
+public class TestTrivialForDynamicSeeding extends SystemTest {
+
+	public static final double defaultDynamicPool = Properties.DYNAMIC_POOL;
+
+	@After
+	public void resetProperties() {
+		Properties.DYNAMIC_POOL = defaultDynamicPool;
+	}
+
+	@Test
+	public void testConcatenatedStringEquals() {
+		EvoSuite evosuite = new EvoSuite();
+
+		String targetClass = TrivialForDynamicSeeding.class.getCanonicalName();
+
+		Properties.TARGET_CLASS = targetClass;
+		Properties.DYNAMIC_POOL = 1d / 3d;
+
+		String[] command = new String[] { "-generateSuite", "-class", targetClass };
+
+		Object result = evosuite.parseCommandLine(command);
+
+		Assert.assertTrue(result != null);
+		Assert.assertTrue("Invalid result type :" + result.getClass(),
+		                  result instanceof GeneticAlgorithm);
+
+		GeneticAlgorithm<?> ga = (GeneticAlgorithm<?>) result;
+		TestSuiteChromosome best = (TestSuiteChromosome) ga.getBestIndividual();
+		System.out.println("EvolvedTestSuite:\n" + best);
+
+		Assert.assertEquals("Non-optimal coverage: ", 1d, best.getCoverage(), 0.001);
+	}
+
+	@Test
+	public void testConcatenatedStringEqualsWithoutSeeding() {
+		EvoSuite evosuite = new EvoSuite();
+
+		String targetClass = TrivialForDynamicSeeding.class.getCanonicalName();
+
+		Properties.TARGET_CLASS = targetClass;
+		Properties.DYNAMIC_POOL = 0.0;
+		ConstantPoolManager.getInstance().reset();
+
+		String[] command = new String[] { "-generateSuite", "-class", targetClass, "-Ddynamic_pool=0.0" };
+
+		Object result = evosuite.parseCommandLine(command);
+
+		Assert.assertTrue(result != null);
+		Assert.assertTrue("Invalid result type :" + result.getClass(),
+		                  result instanceof GeneticAlgorithm);
+
+		GeneticAlgorithm<?> ga = (GeneticAlgorithm<?>) result;
+		TestSuiteChromosome best = (TestSuiteChromosome) ga.getBestIndividual();
+		System.out.println("EvolvedTestSuite:\n" + best);
+
+		Assert.assertEquals("Unexpected coverage: ", 2d / 3d, best.getCoverage(), 0.001);
+	}
+}
