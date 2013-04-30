aefb33f8457f86b96752b2ff56f81de464ef336a
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2013-04-30 07:10:28 +0100

    Remove obsolete properties

---
+++ src/main/java/org/evosuite/assertion/AssertionGenerator.java
@@ -0,0 +1,166 @@
+/**
+ * Copyright (C) 2011,2012 Gordon Fraser, Andrea Arcuri and EvoSuite
+ * contributors
+ * 
+ * This file is part of EvoSuite.
+ * 
+ * EvoSuite is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ * 
+ * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Public License for more details.
+ * 
+ * You should have received a copy of the GNU Public License along with
+ * EvoSuite. If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * 
+ */
+package org.evosuite.assertion;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.evosuite.ga.stoppingconditions.MaxStatementsStoppingCondition;
+import org.evosuite.testcase.ExecutionResult;
+import org.evosuite.testcase.TestCase;
+import org.evosuite.testcase.TestCaseExecutor;
+import org.evosuite.testcase.TestChromosome;
+import org.evosuite.testsuite.TestSuiteChromosome;
+import org.evosuite.utils.Randomness;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * <p>
+ * Abstract AssertionGenerator class.
+ * </p>
+ * 
+ * @author Gordon Fraser
+ */
+public abstract class AssertionGenerator {
+
+	/** Constant <code>logger</code> */
+	protected static final Logger logger = LoggerFactory.getLogger(AssertionGenerator.class);
+
+	/** Constant <code>primitive_observer</code> */
+	protected static final PrimitiveTraceObserver primitive_observer = new PrimitiveTraceObserver();
+
+	/** Constant <code>comparison_observer</code> */
+	protected static final ComparisonTraceObserver comparison_observer = new ComparisonTraceObserver();
+
+	/** Constant <code>same_observer</code> */
+	protected static final SameTraceObserver same_observer = new SameTraceObserver();
+
+	/** Constant <code>inspector_observer</code> */
+	protected static final InspectorTraceObserver inspector_observer = new InspectorTraceObserver();
+
+	/** Constant <code>field_observer</code> */
+	protected static final PrimitiveFieldTraceObserver field_observer = new PrimitiveFieldTraceObserver();
+
+	/** Constant <code>null_observer</code> */
+	protected static final NullTraceObserver null_observer = new NullTraceObserver();
+
+	/**
+	 * <p>
+	 * Constructor for AssertionGenerator.
+	 * </p>
+	 */
+	public AssertionGenerator() {
+		TestCaseExecutor.getInstance().addObserver(primitive_observer);
+		TestCaseExecutor.getInstance().addObserver(comparison_observer);
+		TestCaseExecutor.getInstance().addObserver(inspector_observer);
+		TestCaseExecutor.getInstance().addObserver(field_observer);
+		TestCaseExecutor.getInstance().addObserver(null_observer);
+		TestCaseExecutor.getInstance().addObserver(same_observer);
+	}
+
+	/**
+	 * <p>
+	 * addAssertions
+	 * </p>
+	 * 
+	 * @param test
+	 *            a {@link org.evosuite.testcase.TestCase} object.
+	 */
+	public abstract void addAssertions(TestCase test);
+
+	/**
+	 * Execute a test case on the original unit
+	 * 
+	 * @param test
+	 *            The test case that should be executed
+	 * @return a {@link org.evosuite.testcase.ExecutionResult} object.
+	 */
+	protected ExecutionResult runTest(TestCase test) {
+		ExecutionResult result = new ExecutionResult(test);
+		try {
+			logger.debug("Executing test");
+			result = TestCaseExecutor.getInstance().execute(test);
+			int num = test.size();
+			MaxStatementsStoppingCondition.statementsExecuted(num);
+			result.setTrace(comparison_observer.getTrace(), ComparisonTraceEntry.class);
+			result.setTrace(primitive_observer.getTrace(), PrimitiveTraceEntry.class);
+			result.setTrace(inspector_observer.getTrace(), InspectorTraceEntry.class);
+			result.setTrace(field_observer.getTrace(), PrimitiveFieldTraceEntry.class);
+			result.setTrace(null_observer.getTrace(), NullTraceEntry.class);
+			result.setTrace(same_observer.getTrace(), SameTraceEntry.class);
+		} catch (Exception e) {
+			throw new Error(e);
+		}
+
+		return result;
+	}
+	
+	protected void filterFailingAssertions(TestCase test) {
+		
+		// Make sure we are not keeping assertions influenced by static state
+		// TODO: Need to handle statically initialized classes
+		ExecutionResult result = runTest(test);
+		Set<Assertion> invalidAssertions = new HashSet<Assertion>();
+		for(Assertion assertion : test.getAssertions()) {
+			for(OutputTrace<?> outputTrace : result.getTraces()) {
+				if(outputTrace.isDetectedBy(assertion)) {
+					invalidAssertions.add(assertion);
+					break;
+				}
+			}
+		}
+		logger.info("Removing {} nondeterministic assertions", invalidAssertions.size());
+		for(Assertion assertion : invalidAssertions) {
+			test.removeAssertion(assertion);
+		}
+	}
+
+	public void filterFailingAssertions(List<TestCase> testCases) {
+		List<TestCase> tests = new ArrayList<TestCase>();
+		tests.addAll(testCases);
+		for(TestCase test : tests) {
+			filterFailingAssertions(test);
+		}
+		
+		// Execute again in different order 
+		Randomness.shuffle(tests);
+		for(TestCase test : tests) {
+			filterFailingAssertions(test);
+		}		
+	}
+	
+	public void filterFailingAssertions(TestSuiteChromosome testSuite) {
+		List<TestChromosome> tests = testSuite.getTestChromosomes();
+		for(TestChromosome test : tests) {
+			filterFailingAssertions(test.getTestCase());
+		}
+		
+		// Execute again in different order 
+		Randomness.shuffle(tests);
+		for(TestChromosome test : tests) {
+			filterFailingAssertions(test.getTestCase());
+		}
+	}
+	
+}
---
+++ src/main/java/org/evosuite/sandbox/Sandbox.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (C) 2011,2012 Gordon Fraser, Andrea Arcuri and EvoSuite
+ * contributors
+ * 
+ * This file is part of EvoSuite.
+ * 
+ * EvoSuite is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ * 
+ * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Public License for more details.
+ * 
+ * You should have received a copy of the GNU Public License along with
+ * EvoSuite. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.evosuite.sandbox;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Class which controls enabling and disabling sandbox.
+ * 
+ * 
+ * Note: for the moment it sets a custom security manager and a mocking framework.
+ * But this latter is not really used now (but we might want to use it in the future once fixed).
+ * So, for the time being, this class is just a wrapper over the security manager
+ * 
+ * @author Andrey Tarasevich
+ */
+public class Sandbox {
+
+	private static Logger logger = LoggerFactory.getLogger(Sandbox.class);
+	
+	private static MSecurityManager manager;
+	
+	
+	/**
+	 * Create and initialize security manager for SUT
+	 */
+	public static void initializeSecurityManagerForSUT(){
+		if(manager==null){
+			manager = new MSecurityManager();
+			manager.makePriviligedAllCurrentThreads();			
+			manager.apply();
+		} else {
+			logger.warn("Sandbox can be initalized only once");
+		}
+	}
+	
+	public static void addPriviligedThread(Thread t) {
+		if(manager != null)
+			manager.addPrivilegedThread(t);
+	}
+	
+	public static void resetDefaultSecurityManager() {
+		if(manager!=null){
+			manager.restoreDefaultManager();
+		}
+		manager = null;
+	}
+	
+	
+	public static boolean isSecurityManagerInitialized(){
+		return manager!=null; 
+	}
+	
+	public static void goingToExecuteSUTCode(){
+		if(!isSecurityManagerInitialized()){return;}
+		manager.goingToExecuteTestCase();
+	}
+	
+	public static void doneWithExecutingSUTCode(){
+		if(!isSecurityManagerInitialized()){return;}
+		manager.goingToEndTestCase();
+	}
+	
+	
+	public static void goingToExecuteUnsafeCodeOnSameThread() throws SecurityException, IllegalStateException {
+		if(!isSecurityManagerInitialized()){return;}
+		manager.goingToExecuteUnsafeCodeOnSameThread();
+	}
+
+	public static void doneWithExecutingUnsafeCodeOnSameThread() throws SecurityException, IllegalStateException {
+		if(!isSecurityManagerInitialized()){return;}
+		manager.doneWithExecutingUnsafeCodeOnSameThread();
+	}
+}
