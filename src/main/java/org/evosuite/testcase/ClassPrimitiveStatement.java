b70a0c4210185ff6de0d6abf3e5525891fab37a8
Andrea Arcuri <arcuri82@gmail.com>
2013-05-01 15:39:52 +0200

    fix for failing test case

---
+++ src/main/java/org/evosuite/testcase/ClassPrimitiveStatement.java
@@ -0,0 +1,77 @@
+package org.evosuite.testcase;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import org.evosuite.Properties;
+import org.evosuite.TestGenerationContext;
+import org.evosuite.primitives.ConstantPoolManager;
+import org.evosuite.utils.GenericClass;
+import org.objectweb.asm.commons.GeneratorAdapter;
+
+public class ClassPrimitiveStatement extends PrimitiveStatement<Class<?>> {
+
+	private static final long serialVersionUID = -2728777640255424791L;
+
+	public ClassPrimitiveStatement(TestCase tc, Class<?> value) {
+		super(tc, new GenericClass(Class.class).getWithWildcardTypes(), value);
+	}
+
+	public ClassPrimitiveStatement(TestCase tc) {
+		super(tc, new GenericClass(Class.class).getWithWildcardTypes(), Properties.getTargetClass());
+	}
+
+	@Override
+	public void delta() {
+		randomize();
+	}
+
+	@Override
+	public void zero() {
+		this.value = Properties.getTargetClass();
+	}
+
+	@Override
+	protected void pushBytecode(GeneratorAdapter mg) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void randomize() {
+		org.objectweb.asm.Type type = ConstantPoolManager.getInstance().getConstantPool().getRandomType();
+		try {
+			value = Class.forName(type.getClassName(), true,
+			                      TestGenerationContext.getClassLoader());
+		} catch (ClassNotFoundException e) {
+			logger.warn("Error loading class: " + e);
+		}
+	}
+
+	@Override
+	public void changeClassLoader(ClassLoader loader) {
+		Class<?> currentClass = value;
+		try {
+			value = loader.loadClass(currentClass.getCanonicalName());
+		} catch (ClassNotFoundException e) {
+			logger.warn("Could not load class in new classloader: " + currentClass);
+		}
+	}
+
+	private void writeObject(ObjectOutputStream oos) throws IOException {
+		Class<?> currentClass = value;
+		oos.writeObject(currentClass.getName());
+	}
+
+	private void readObject(ObjectInputStream ois) throws ClassNotFoundException,
+	        IOException {
+		String name = (String) ois.readObject();
+		try {
+			value = TestGenerationContext.getClassLoader().loadClass(name);
+		} catch (ClassNotFoundException e) {
+			logger.warn("Could not load class in new classloader: " + name);
+		}
+
+	}
+}
---
+++ src/test/java/org/evosuite/SystemTest.java
@@ -0,0 +1,141 @@
+/**
+ * Copyright (C) 2011,2012 Gordon Fraser, Andrea Arcuri and EvoSuite
+ * contributors
+ * 
+ * This file is part of EvoSuite.
+ * 
+ * EvoSuite is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ * 
+ * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Public License for more details.
+ * 
+ * You should have received a copy of the GNU Public License along with
+ * EvoSuite. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.evosuite;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.Permission;
+
+import org.evosuite.Properties.StoppingCondition;
+import org.evosuite.utils.LoggingUtils;
+import org.evosuite.utils.Randomness;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+
+/**
+ * @author Andrea Arcuri
+ * 
+ */
+public class SystemTest {
+
+	public static final String ALREADY_SETUP = "systemtest.alreadysetup";
+
+	private static java.util.Properties currentProperties;
+
+	static {
+		String s = System.getProperty(ALREADY_SETUP);
+		if (s == null) {
+			System.setProperty(ALREADY_SETUP, ALREADY_SETUP);
+			runSetup();
+		}
+	}
+
+
+	@After
+	public void resetStaticVariables() {
+		TestGenerationContext.getInstance().resetContext();
+		System.setProperties(currentProperties); 
+	}
+
+	@Before
+	public void setDefaultPropertiesForTestCases() {
+		Properties.HTML = false;
+		Properties.SHOW_PROGRESS = false;
+		Properties.SERIALIZE_RESULT = false;
+		Properties.JUNIT_TESTS = false;
+		Properties.PLOT = false;
+
+		Properties.STOPPING_CONDITION = StoppingCondition.MAXSTATEMENTS;
+		Properties.SEARCH_BUDGET = 10000;
+
+		Properties.GLOBAL_TIMEOUT = 50;
+		Properties.MINIMIZATION_TIMEOUT = 8;
+		Properties.EXTRA_TIMEOUT = 2;
+
+		Properties.CLIENT_ON_THREAD = true;
+		Properties.SANDBOX = false;
+		Properties.ERROR_BRANCHES = false;
+
+		TestGenerationContext.getInstance().resetContext();
+		Randomness.setSeed(42);
+
+		currentProperties = (java.util.Properties) System.getProperties().clone();
+	}
+
+	/*
+	 * this static variable is a safety net to be sure it is called only once. 
+	 * static variables are shared and not re-initialized
+	 * during a sequence of test cases.
+	 */
+	private static boolean hasBeenAlreadyRun = false;
+
+	private static void runSetup() {
+		if (hasBeenAlreadyRun) {
+			return;
+		}
+
+		LoggingUtils.checkAndSetLogLevel();
+
+		deleteEvoDirs();
+
+		System.out.println("*** SystemTest: runSetup() ***");
+
+		String target = System.getProperty("user.dir") + File.separator + "target"
+				+ File.separator + "test-classes";
+
+		File targetDir = new File(target);
+		try {
+			Assert.assertTrue("Target directory does not exist: "
+					+ targetDir.getCanonicalPath(), targetDir.exists());
+		} catch (IOException e) {
+			Assert.fail(e.getMessage());
+		}
+		Assert.assertTrue(targetDir.isDirectory());
+
+
+		EvoSuite evosuite = new EvoSuite();
+		String[] command = new String[] {
+				"-setup", target };
+
+		Object result = evosuite.parseCommandLine(command);
+		Assert.assertNull(result);
+		File evoProp = new File(Properties.OUTPUT_DIR + File.separator
+				+ "evosuite.properties");
+		Assert.assertTrue("It was not created: " + evoProp.getAbsolutePath(),
+				evoProp.exists());
+
+		hasBeenAlreadyRun = true;
+	}
+
+
+	private static void deleteEvoDirs() {
+
+		System.out.println("*** SystemTest: deleteEvoDirs() ***");
+
+		try {
+			org.apache.commons.io.FileUtils.deleteDirectory(new File("evosuite-files"));
+			org.apache.commons.io.FileUtils.deleteDirectory(new File("evosuite-report"));
+			org.apache.commons.io.FileUtils.deleteDirectory(new File("evosuite-tests"));
+		} catch (IOException e) {
+			Assert.fail(e.getMessage());
+		}
+		hasBeenAlreadyRun = false;
+	}
+}
