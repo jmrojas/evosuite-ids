9112fe91a9ef85b2412fdffe87f192a1c8b3b519
zecarlos <zecarlos@wifi-interno.fe.up.pt>
2013-05-27 10:52:25 +0100

    EntropyCoverage based on statements instead of byte code instructions

--- src/main/java/org/evosuite/coverage/entropy/EntropyCoverageTestFitness.java
+++ src/main/java/org/evosuite/coverage/entropy/EntropyCoverageTestFitness.java
@@ -1,13 +1,8 @@
 package org.evosuite.coverage.entropy;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
 import org.evosuite.coverage.branch.BranchCoverageFactory;
 import org.evosuite.coverage.branch.BranchCoverageTestFitness;
 import org.evosuite.graphs.cfg.BytecodeInstruction;
-import org.evosuite.graphs.cfg.ControlDependency;
 import org.evosuite.testcase.ExecutionResult;
 import org.evosuite.testcase.TestChromosome;
 import org.evosuite.testcase.TestFitnessFunction;
@@ -25,31 +20,14 @@ public class EntropyCoverageTestFitness extends
 	private static int			component_index;
 
 	protected BytecodeInstruction goalInstruction;
-	protected List<BranchCoverageTestFitness> branchFitnesses = new ArrayList<BranchCoverageTestFitness>();
+	protected BranchCoverageTestFitness branchFitnesses;
 
 	public EntropyCoverageTestFitness(BytecodeInstruction goalInstruction) {
 		if (goalInstruction == null)
 			throw new IllegalArgumentException("null given");
 
 		this.goalInstruction = goalInstruction;
-
-		Set<ControlDependency> cds = goalInstruction.getControlDependencies();
-		for (ControlDependency cd : cds) {
-			BranchCoverageTestFitness fitness = BranchCoverageFactory.createBranchCoverageTestFitness(cd);
-			branchFitnesses.add(fitness);
-		}
-
-		if (goalInstruction.isRootBranchDependent())
-			branchFitnesses.add(BranchCoverageFactory.createRootBranchTestFitness(goalInstruction));
-
-		if (cds.isEmpty() && !goalInstruction.isRootBranchDependent())
-			throw new IllegalStateException(
-			        "expect control dependencies to be empty only for root dependent instructions: "
-			                + toString());
-
-		if (branchFitnesses.isEmpty())
-			throw new IllegalStateException(
-			        "an instruction is at least on the root branch of it's method");
+		branchFitnesses = BranchCoverageFactory.createRootBranchTestFitness(this.goalInstruction);
 	}
 
 	@Override
@@ -57,14 +35,10 @@ public class EntropyCoverageTestFitness extends
 	{
 		double touch = 0.0;
 
-		for (TestFitnessFunction goal : branchFitnesses)
-		{
-			if (goal.isCovered(result)) {
-				touch = 1.0;
-				if (isToSaveCoverage)
-					test_coverage[component_index] = true;
-				break ;
-			}
+		if (branchFitnesses.isCovered(result)) {
+			touch = 1.0;
+			if (isToSaveCoverage)
+				test_coverage[component_index] = true;
 		}
 
 		component_index++;
