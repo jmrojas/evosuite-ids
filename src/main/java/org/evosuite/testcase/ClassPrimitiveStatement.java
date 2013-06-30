5d1aab9ab522b1fca730cdf3b82197a949a5f314
gordon <gordon@localhost>
2013-06-30 22:38:43 +0100

    Add test case for carver and evosuite runner

---
+++ src/main/java/org/evosuite/testcase/ClassPrimitiveStatement.java
@@ -0,0 +1,110 @@
+package org.evosuite.testcase;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Array;
+
+import org.evosuite.Properties;
+import org.evosuite.TestGenerationContext;
+import org.evosuite.primitives.ConstantPoolManager;
+import org.evosuite.utils.GenericClass;
+import org.objectweb.asm.commons.GeneratorAdapter;
+
+public class ClassPrimitiveStatement extends PrimitiveStatement<Class<?>> {
+
+	private static final long serialVersionUID = -2728777640255424791L;
+
+	public ClassPrimitiveStatement(TestCase tc, Class<?> value) {
+		super(tc, new GenericClass(Class.class).getWithWildcardTypes(), value);
+	}
+
+	public ClassPrimitiveStatement(TestCase tc) {
+		super(tc, new GenericClass(Class.class).getWithWildcardTypes(),
+		        Properties.getTargetClass());
+	}
+
+	@Override
+	public void delta() {
+		randomize();
+	}
+
+	@Override
+	public void zero() {
+		this.value = Properties.getTargetClass();
+	}
+
+	@Override
+	protected void pushBytecode(GeneratorAdapter mg) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private Class<?> getType(org.objectweb.asm.Type type) throws ClassNotFoundException {
+		// Not quite sure why we have to treat primitives explicitly...
+		switch(type.getSort()) {
+		case org.objectweb.asm.Type.ARRAY:
+			org.objectweb.asm.Type componentType = type.getElementType();
+			Class<?> componentClass = getType(componentType);
+			Class<?> arrayClass = Array.newInstance(componentClass, 0).getClass();
+			return arrayClass;
+		case org.objectweb.asm.Type.BOOLEAN:
+			return boolean.class;
+		case org.objectweb.asm.Type.BYTE:
+			return byte.class;
+		case org.objectweb.asm.Type.CHAR:
+			return char.class;
+		case org.objectweb.asm.Type.DOUBLE:
+			return double.class;
+		case org.objectweb.asm.Type.FLOAT:
+			return float.class;
+		case org.objectweb.asm.Type.INT:
+			return int.class;
+		case org.objectweb.asm.Type.LONG:
+			return long.class;
+		case org.objectweb.asm.Type.SHORT:
+			return short.class;
+		default:
+			return Class.forName(type.getClassName(), true,
+					TestGenerationContext.getClassLoader());
+		}
+	}
+	
+	@Override
+	public void randomize() {
+		org.objectweb.asm.Type type = ConstantPoolManager.getInstance().getConstantPool().getRandomType();
+		try {
+			value = getType(type);
+		} catch (ClassNotFoundException e) {
+			logger.warn("Error loading class " + type.getClassName() + ": " + e);
+		} catch (NoClassDefFoundError e) {
+			logger.warn("Error loading class " + type.getClassName() + ": " + e);
+		}
+	}
+
+	@Override
+	public void changeClassLoader(ClassLoader loader) {
+		Class<?> currentClass = value;
+		try {
+			value = loader.loadClass(currentClass.getCanonicalName());
+		} catch (ClassNotFoundException e) {
+			logger.warn("Could not load class in new classloader: " + currentClass);
+		}
+	}
+
+	private void writeObject(ObjectOutputStream oos) throws IOException {
+		Class<?> currentClass = value;
+		oos.writeObject(currentClass.getName());
+	}
+
+	private void readObject(ObjectInputStream ois) throws ClassNotFoundException,
+	        IOException {
+		String name = (String) ois.readObject();
+		try {
+			value = TestGenerationContext.getClassLoader().loadClass(name);
+		} catch (ClassNotFoundException e) {
+			logger.warn("Could not load class in new classloader: " + name);
+		}
+
+	}
+}
