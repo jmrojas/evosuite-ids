6ac38d3e1cf2cee8c6e037dd7a2d33352f334974
zecarlos <zecarlos@joses-imac.local>
2013-07-04 20:06:42 +0100

    Fixed Entropy fitness

--- src/main/java/org/evosuite/coverage/entropy/EntropyCoverageTestFitness.java
+++ src/main/java/org/evosuite/coverage/entropy/EntropyCoverageTestFitness.java
@@ -1,8 +1,13 @@
 package org.evosuite.coverage.entropy;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import org.evosuite.coverage.branch.BranchCoverageFactory;
 import org.evosuite.coverage.branch.BranchCoverageTestFitness;
 import org.evosuite.graphs.cfg.BytecodeInstruction;
+import org.evosuite.graphs.cfg.ControlDependency;
 import org.evosuite.testcase.ExecutionResult;
 import org.evosuite.testcase.TestChromosome;
 import org.evosuite.testcase.TestFitnessFunction;
@@ -19,15 +24,32 @@ public class EntropyCoverageTestFitness extends
 	private static boolean[]	test_coverage;
 	private static int			component_index;
 
-	protected BytecodeInstruction		goalInstruction;
-	protected BranchCoverageTestFitness	branchFitnesses;
+	protected BytecodeInstruction				goalInstruction;
+	protected List<BranchCoverageTestFitness>	branchFitnesses = new ArrayList<BranchCoverageTestFitness>();
 
 	public EntropyCoverageTestFitness(BytecodeInstruction goalInstruction) {
 		if (goalInstruction == null)
 			throw new IllegalArgumentException("null given");
 
 		this.goalInstruction = goalInstruction;
-		branchFitnesses = BranchCoverageFactory.createRootBranchTestFitness(this.goalInstruction);
+
+		Set<ControlDependency> cds = goalInstruction.getControlDependencies();
+		for (ControlDependency cd : cds) {
+			BranchCoverageTestFitness fitness = BranchCoverageFactory.createBranchCoverageTestFitness(cd);
+			branchFitnesses.add(fitness);
+		}
+
+		if (goalInstruction.isRootBranchDependent())
+			branchFitnesses.add(BranchCoverageFactory.createRootBranchTestFitness(goalInstruction));
+
+		if (cds.isEmpty() && !goalInstruction.isRootBranchDependent())
+			throw new IllegalStateException(
+			        "expect control dependencies to be empty only for root dependent instructions: "
+			                + toString());
+
+		if (branchFitnesses.isEmpty())
+			throw new IllegalStateException(
+			        "an instruction is at least on the root branch of it's method");
 	}
 
 	@Override
@@ -35,10 +57,12 @@ public class EntropyCoverageTestFitness extends
 	{
 		double touch = 0.0;
 
-		if (branchFitnesses.isCovered(result)) {
-			touch = 1.0;
-			if (isToSaveCoverage)
-				test_coverage[component_index] = true;
+		for (BranchCoverageTestFitness branchFitness : branchFitnesses) {
+			if (branchFitness.isCovered(result)) {
+				touch = 1.0;
+				if (isToSaveCoverage)
+					test_coverage[component_index] = true;
+			}
 		}
 
 		component_index++;
