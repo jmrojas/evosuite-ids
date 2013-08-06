91fc4ecdf96388eddaaa681ddbc2dee03ee7b844
arcuri <arcuri@localhost>
2013-08-06 19:01:02 +0200

    adding test cases and fixing CTG scheduler

---
+++ src/main/java/org/evosuite/continuous/job/schedule/SimpleSchedule.java
@@ -0,0 +1,44 @@
+package org.evosuite.continuous.job.schedule;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.evosuite.continuous.job.JobDefinition;
+import org.evosuite.continuous.job.JobScheduler;
+import org.evosuite.continuous.project.ProjectStaticData;
+import org.evosuite.continuous.project.ProjectStaticData.ClassInfo;
+
+public class SimpleSchedule extends OneTimeSchedule{
+
+	public SimpleSchedule(JobScheduler scheduler){
+		super(scheduler);
+	}
+
+	@Override
+	protected List<JobDefinition> createScheduleOnce() {
+
+		assert enoughBudgetForAll(); 
+
+		ProjectStaticData data = scheduler.getProjectData();
+		int totalBudget = 60 * scheduler.getTotalBudgetInMinutes() * super.getNumberOfUsableCores(); 
+
+		List<JobDefinition> jobs = new LinkedList<JobDefinition>();
+
+		//simple case, distribute budget equally
+		int cores = super.getNumberOfUsableCores();
+		int cuts = data.getTotalNumberOfTestableCUTs();		
+		int slots = (int)Math.round(cores * Math.ceil((double) cuts / (double) cores));
+		int budgetInSecondsPerCUT = totalBudget / slots; 
+
+		for(ClassInfo info : data.getClassInfos()){
+			if(!info.isTestable()){
+				continue;
+			}
+			JobDefinition job = new JobDefinition(
+					budgetInSecondsPerCUT, getConstantMemoryPerJob(), info.getClassName(), 0, null);
+			jobs.add(job);
+		}
+		return jobs;
+	}
+
+}
