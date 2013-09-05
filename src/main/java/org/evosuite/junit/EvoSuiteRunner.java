944b8b944bfd4d1cc8a2c65d6ff3ec8f20103b1b
gordon <gordon@localhost>
2013-09-05 17:34:05 +0100

    Fix the fix for the inheritance tree

---
+++ src/main/java/org/evosuite/junit/EvoSuiteRunner.java
@@ -0,0 +1,57 @@
+package org.evosuite.junit;
+
+import java.io.InputStream;
+
+import org.evosuite.EvoSuite;
+import org.evosuite.instrumentation.InstrumentingClassLoader;
+import org.evosuite.utils.LoggingUtils;
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.junit.runners.model.InitializationError;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
+import ch.qos.logback.core.util.StatusPrinter;
+
+/**
+ * @deprecated 
+ *
+ * Should rather use InstrumentingAgent
+ */
+public class EvoSuiteRunner extends BlockJUnit4ClassRunner {
+
+	
+	public EvoSuiteRunner(Class<?> klass) throws InitializationError {
+		super(getFromEvoSuiteClassloader(klass));
+	}
+
+	
+	private static Class<?> getFromEvoSuiteClassloader(Class<?> clazz) throws InitializationError {
+	    try {
+	    	/*
+	    	 *  properties like REPLACE_CALLS will be set directly in the JUnit files
+	    	 */
+
+	    	LoggingUtils.loadLogbackForEvoSuite();
+
+	    	/*
+	    	 * TODO: this approach does throw away all the possible instrumentation done on the input clazz,
+	    	 * eg code coverage of Emma, Cobertura, Javalanche, etc.
+	    	 * 
+	    	 * maybe an option would be to use java agents:
+	    	 * 
+	    	 * http://dhruba.name/2010/02/07/creation-dynamic-loading-and-instrumentation-with-javaagents/
+	    	 * http://www.eclemma.org/jacoco/trunk/doc/implementation.html
+	    	 * http://osi.fotap.org/2008/06/27/dynamically-installing-agents-in-java-6/
+	    	 * http://docs.oracle.com/javase/7/docs/api/java/lang/instrument/package-summary.html
+	    	 */
+	    	
+	    	InstrumentingClassLoader classLoader = new InstrumentingClassLoader();
+	        return Class.forName(clazz.getName(), true, classLoader);
+	    } catch (ClassNotFoundException e) {
+	        throw new InitializationError(e);
+	    }
+	}
+	
+}
---
+++ src/main/java/org/evosuite/utils/SystemInUtil.java
@@ -0,0 +1,162 @@
+package org.evosuite.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.evosuite.Properties;
+import org.evosuite.setup.TestCluster;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This singleton class is used to handle calls to System.in by
+ * replacing them with a smart stub
+ * 
+ * @author arcuri
+ *
+ */
+public class SystemInUtil extends InputStream{
+
+	public final InputStream defaultIn = System.in;
+
+	private static Logger logger = LoggerFactory.getLogger(SystemInUtil.class);
+
+	private static volatile SystemInUtil singleton = new SystemInUtil();
+
+	/**
+	 * Has System.in ever be used by the SUT?
+	 */
+	private volatile boolean beingUsed;
+
+	/**
+	 * The data that will be taken from System.in
+	 */
+	private volatile List<Byte> data;
+
+	private volatile AtomicInteger counter;
+
+	private boolean hasAddedSupport;
+
+	/**
+	 * Hidden constructor
+	 */
+	protected SystemInUtil(){
+		super();
+		beingUsed = false;
+		if(Properties.REPLACE_SYSTEM_IN){
+			System.setIn(this);
+		}
+	}
+
+	public static synchronized SystemInUtil getInstance(){
+		return singleton;
+	}
+
+	/**
+	 * Reset the static state be re-instantiate the singleton
+	 */
+	public static synchronized void resetSingleton(){
+		singleton = new SystemInUtil();
+	}
+
+	/**
+	 * Setup mocked/stubbed System.in for the test case
+	 */
+	public void initForTestCase(){
+		data = new ArrayList<>();
+		counter = new AtomicInteger(0);
+	}
+
+	/**
+	 * Use given <code>input</code> string to represent the data
+	 * that will be provided by System.in.
+	 * The string will be appended to current buffer as a new line,
+	 * i.e. by adding "\n" to the <code>input</code> string
+	 * 
+	 * @param input	A string representing an input on the console
+	 */
+	public static void addInputLine(String input){
+		if(input==null){
+			return;
+		} 
+
+		/*
+		 * Note: this method needs to be static, as we call it directly in the test cases.
+		 */
+
+		synchronized(singleton.data){
+			String line = input+"\n";
+			for(byte b : line.getBytes()){
+				singleton.data.add((Byte)b);
+			}		
+		}
+	}
+
+	/**
+	 * If System.in was used, add methods to handle/simulate it
+	 */
+	public void addSupportInTestClusterIfNeeded(){
+		if(!beingUsed || hasAddedSupport){
+			return;
+		}
+
+		logger.debug("Going to add support for System.in");
+		hasAddedSupport = true;
+
+		try {
+			TestCluster.getInstance().addTestCall(new GenericMethod(
+					SystemInUtil.class.getMethod("addInputLine",new Class<?>[] { String.class }),
+					new GenericClass(SystemInUtil.class)));
+		} catch (SecurityException e) {
+			logger.error("Error while handling Random: "+e.getMessage(),e);
+		} catch (NoSuchMethodException e) {
+			logger.error("Error while handling Random: "+e.getMessage(),e);
+		}
+	}
+
+
+	@Override
+	public int read() throws IOException {
+
+		beingUsed = true;
+
+		int i = counter.getAndIncrement();
+
+		if(i==data.size()){
+			//first time we reach end of buffer, we return -1 to represents its end
+			return -1; 
+		}
+
+		if(i>data.size()){
+			/*
+			 * this is bit tricky situation.
+			 * if we arrive here, it means that SUT has already asked for read(),
+			 * got a -1, but then keep asking it.
+			 * To avoid infinite loops and hard to kill threads, here we throw an
+			 * IO exception (which would still be as part of normal behavior of read())
+			 */
+			throw new IOException("Asked to read from System.in when test case has decided to simulate an IO exception");
+		}
+
+		/*
+		 * Note: it is important here that this read() is not blocking.
+		 * 
+		 * TODO: if needed, we could have a delay here to simulate a blocking
+		 * operation till new data is provided to System.in.
+		 * But it is likely not going to be so useful for unit testing
+		 */
+		return (int) data.get(i);
+	}
+
+	/**
+	 * Has there be any call to System.in.read()?
+	 * @return
+	 */
+	public boolean hasBeenUsed() {
+		return beingUsed;
+	}
+
+}
