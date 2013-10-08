79509199aa6971961bf02a72ffc1275439584174
Andrea Arcuri <arcuri82@gmail.com>
2013-10-08 09:20:01 +0200

    added check to remove test cases that do not compile

---
+++ src/main/java/org/evosuite/rmi/service/MasterNodeRemote.java
@@ -0,0 +1,29 @@
+package org.evosuite.rmi.service;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import org.evosuite.ga.Chromosome;
+
+/**
+ * Master Node view in the client process.  
+ * @author arcuri
+ *
+ */
+public interface MasterNodeRemote extends Remote {
+
+	public static final String RMI_SERVICE_NAME = "MasterNode";
+	
+	/*
+	 * Note: we need names starting with 'evosuite' here, becase those names are accessed 
+	 * through reflactions and used in the checks of the sandbox 
+	 */
+	
+	public void evosuite_registerClientNode(String clientRmiIdentifier) throws RemoteException;
+	
+	public void evosuite_informChangeOfStateInClient(String clientRmiIdentifier, ClientState state, ClientStateInformation information) throws RemoteException;
+	
+	public void evosuite_collectStatistics(String clientRmiIdentifier, Chromosome individual) throws RemoteException;
+
+	public void evosuite_collectStatistics(String clientRmiIdentifier, String name, Object value) throws RemoteException;
+}
