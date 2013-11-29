0135ff833bb2262c1a92480301fff752fd254733
gordon <gordon@localhost>
2013-11-29 20:42:13 +0000

    Fix for static state in search statistics

---
+++ src/main/java/org/evosuite/testcarver/extraction/CarvingRunListener.java
@@ -0,0 +1,75 @@
+package org.evosuite.testcarver.extraction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.evosuite.Properties;
+import org.evosuite.TestGenerationContext;
+import org.evosuite.testcarver.capture.CaptureLog;
+import org.evosuite.testcarver.capture.Capturer;
+import org.evosuite.testcarver.codegen.CaptureLogAnalyzer;
+import org.evosuite.testcarver.testcase.EvoTestCaseCodeGenerator;
+import org.evosuite.testcase.DefaultTestCase;
+import org.evosuite.testcase.TestCase;
+import org.evosuite.utils.GenericTypeInference;
+import org.junit.runner.Description;
+import org.junit.runner.notification.RunListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CarvingRunListener extends RunListener {
+
+	private final List<TestCase> carvedTests = new ArrayList<TestCase>();
+
+	private final static Logger logger = LoggerFactory.getLogger(CarvingRunListener.class);
+
+	private final Class<?>[] targetClasses;
+
+	public CarvingRunListener() {
+		targetClasses = new Class<?>[1];
+		targetClasses[0] = Properties.getTargetClass();
+	}
+
+	public List<TestCase> getTestCases() {
+		return carvedTests;
+	}
+
+	@Override
+	public void testStarted(Description description) throws Exception {
+		Capturer.startCapture();
+	}
+
+	@Override
+	public void testFinished(Description description) throws Exception {
+		final CaptureLog log = Capturer.stopCapture();
+		this.processLog(log);
+		Capturer.clear();
+	}
+
+	/**
+	 * Creates TestCase out of the captured log
+	 * 
+	 * @param log
+	 *            log captured from test execution
+	 */
+	private void processLog(final CaptureLog log) {
+		final CaptureLogAnalyzer analyzer = new CaptureLogAnalyzer();
+		final EvoTestCaseCodeGenerator codeGen = new EvoTestCaseCodeGenerator();
+		analyzer.analyze(log, codeGen, this.targetClasses);
+
+		DefaultTestCase test = (DefaultTestCase) codeGen.getCode();
+		logger.info("Carved test of length " + test.size());
+		try {
+			test.changeClassLoader(TestGenerationContext.getClassLoader());
+			GenericTypeInference inference = new GenericTypeInference();
+			//test.accept(inference);
+			inference.inferTypes(test);
+
+			carvedTests.add(test);
+		} catch (Throwable t) {
+			logger.info("Exception during carving: " + t);
+
+		}
+		codeGen.clear();
+	}
+}
