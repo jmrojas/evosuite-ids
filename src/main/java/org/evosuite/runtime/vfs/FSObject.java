e7dda2211807da21443d6c9555e2dca9dcae6d1f
Juan Galeotti <galeotti@cs.uni-saarland.de>
2014-03-04 12:21:28 +0100

    CheapPurityAnalysis support for INVOKESPECIAL bytecode instructions

---
+++ src/main/java/org/evosuite/runtime/vfs/FSObject.java
@@ -0,0 +1,159 @@
+package org.evosuite.runtime.vfs;
+
+import java.io.File;
+
+import org.evosuite.Properties;
+
+/**
+ * Parent class for both files and folders
+ * 
+ * @author arcuri
+ *
+ */
+public abstract class FSObject {
+
+	private volatile boolean readPermission;
+
+	private volatile boolean writePermission;
+
+	private volatile boolean executePermission;
+
+	/**
+	 * Normalized path uniquely identifying this file on the VFS
+	 */
+	protected volatile String path;
+
+	/**
+	 * The direct parent folder
+	 */
+	protected final VFolder parent;
+
+	/**
+	 * Even if file is removed from file system, some threads could still have
+	 * references to it. So, long/expensive operations could be stopped here if
+	 * file is deleted
+	 */
+	protected volatile boolean deleted;
+
+	protected volatile long lastModified;
+
+	public FSObject(String path,VFolder parent){		
+		readPermission = true;
+		writePermission = true;
+		executePermission = true;
+		this.path = normalizePath(path);	
+		this.parent = parent;
+		this.deleted = false;
+		this.lastModified = getCurrentTimeMillis();
+	}
+
+	protected long getCurrentTimeMillis(){
+		if(Properties.REPLACE_CALLS){
+			return org.evosuite.runtime.System.currentTimeMillis();
+		} else {
+			return java.lang.System.currentTimeMillis();
+		}
+	}
+
+
+	public boolean rename(String newPath){
+
+		if(!isWritePermission() || !parent.isWritePermission()){
+			return false;
+		}
+
+		path = newPath;
+
+		return true; 
+	}
+
+	public boolean delete(){
+		parent.removeChild(getName());
+		deleted = true;
+		return deleted;
+	}
+
+	public boolean isFolder(){
+		return this instanceof VFolder;
+	}
+
+	public String getName(){
+		if(path==null){
+			return null;
+		}
+		return new File(path).getName(); 
+	}
+
+	public String normalizePath(String rawPath){
+		if(rawPath==null){
+			return null;
+		}
+		return new File(rawPath).getAbsolutePath(); 
+	}
+
+	public boolean isReadPermission() {
+		return readPermission;
+	}
+
+
+	public void setReadPermission(boolean readPermission) {
+		this.readPermission = readPermission;
+	}
+
+
+	public boolean isWritePermission() {
+		return writePermission;
+	}
+
+
+	public void setWritePermission(boolean writePermission) {
+		this.writePermission = writePermission;
+	}
+
+
+	public boolean isExecutePermission() {
+		return executePermission;
+	}
+
+
+	public void setExecutePermission(boolean executePermission) {
+		this.executePermission = executePermission;
+	}
+
+	public String getPath() {
+		return path == null ? "" : path;
+	}
+
+	/**
+	 * Once a file/folder is deleted, it shouldn't be accessible any more from the VFS.
+	 * But in case some thread holds a reference to this instance, we need to mark 
+	 * that it is supposed to be deleted
+	 * @return
+	 */
+	public boolean isDeleted() {
+		return deleted;
+	}
+
+	@Override
+	public String toString(){
+		return getPath();
+	}
+
+	public long getLastModified() {
+		return lastModified;
+	}
+
+	public boolean setLastModified(long lastModified) {
+		//TODO check all of its callers, and  if should simulate time
+
+		if(!this.isWritePermission()){
+			return false;
+		}
+		this.lastModified = lastModified;
+		return true;
+	}
+
+	public VFolder getParent() {
+		return parent;
+	}
+}
