89dce147946a67f38b91397b8dc4f6f85e433587
gordon <gordon@localhost>
2014-03-04 22:18:25 +0000

    Comment out serialUID stuff, and try to avoid serialisation in the first place

---
+++ src/main/java/org/evosuite/assertion/CheapPurityAnalyzer.java
@@ -0,0 +1,226 @@
+package org.evosuite.assertion;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
+import org.objectweb.asm.Type;
+
+public class CheapPurityAnalyzer {
+
+	private boolean underApproximateAnalysis = true;
+
+	private final HashSet<MethodEntry> updateFieldMethodList = new HashSet<MethodEntry>();
+	private final HashSet<MethodEntry> notUpdateFieldMethodList = new HashSet<MethodEntry>();
+
+	private final HashSet<MethodEntry> pureMethodCache = new HashSet<MethodEntry>();
+	private final HashSet<MethodEntry> notPureMethodCache = new HashSet<MethodEntry>();
+
+	private static final CheapPurityAnalyzer instance = new CheapPurityAnalyzer();
+
+	public static CheapPurityAnalyzer getInstance() {
+		return instance;
+	}
+
+	public boolean isPure(String className, String methodName, String descriptor) {
+		MethodEntry entry = new MethodEntry(className, methodName, descriptor);
+		return isPure(entry);
+	}
+
+	private boolean isPure(MethodEntry entry) {
+		return isPure(entry, new Stack<MethodEntry>());
+	}
+
+	private boolean isPure(MethodEntry entry, Stack<MethodEntry> callStack) {
+		if (this.pureMethodCache.contains(entry))
+			return true;
+
+		if (this.notPureMethodCache.contains(entry))
+			return false;
+
+		if (this.updateFieldMethodList.contains(entry)) {
+			this.notPureMethodCache.add(entry);
+			return false;
+		}
+
+		if (staticCalls.containsKey(entry)) {
+			Set<MethodEntry> calls = staticCalls.get(entry);
+			for (MethodEntry callMethodEntry : calls) {
+				if (!callStack.contains(callMethodEntry)) {
+					Stack<MethodEntry> copyOfStack = new Stack<MethodEntry>();
+					copyOfStack.addAll(callStack);
+					copyOfStack.add(entry);
+					if (!isPure(callMethodEntry, copyOfStack)) {
+						this.notPureMethodCache.add(entry);
+						return false;
+					}
+				}
+			}
+		}
+
+		if (specialCalls.containsKey(entry)) {
+			return false;
+		}
+
+		if (virtualCalls.containsKey(entry)) {
+			return false;
+		}
+
+		if (interfaceCalls.containsKey(entry)) {
+			return false;
+		}
+
+		if (this.notUpdateFieldMethodList.contains(entry)) {
+			this.pureMethodCache.add(entry);
+			return true;
+		}
+
+		return defaultPurityValue();
+	}
+
+	/**
+	 * We return this value when we can't conclude if a given method is pure or not.
+	 * @return
+	 */
+	private boolean defaultPurityValue() {
+		if (this.underApproximateAnalysis) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public boolean isPure(java.lang.reflect.Method method) {
+		String className = method.getDeclaringClass().getCanonicalName();
+		String methodName = method.getName();
+		String descriptor = Type.getMethodDescriptor(method);
+
+		MethodEntry entry = new MethodEntry(className, methodName, descriptor);
+		return isPure(entry);
+	}
+
+	private static class MethodEntry {
+		private final String className;
+		private final String methodName;
+		private final String descriptor;
+
+		public MethodEntry(String className, String methodName,
+				String descriptor) {
+			this.className = className;
+			this.methodName = methodName;
+			this.descriptor = descriptor;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + className.hashCode();
+			result = prime * result + descriptor.hashCode();
+			result = prime * result + methodName.hashCode();
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			MethodEntry other = (MethodEntry) obj;
+			return (className.equals(other.className) && methodName
+					.equals(other.methodName))
+					&& descriptor.equals(other.descriptor);
+		}
+
+		@Override
+		public String toString() {
+			return "MethodEntry [className=" + className + ", methodName="
+					+ methodName + ", descriptor=" + String.valueOf(descriptor)
+					+ "]";
+		}
+	}
+
+	public void addUpdatesFieldMethod(String className, String methodName,
+			String descriptor) {
+		String classNameWithDots = className.replace("/", ".");
+		MethodEntry entry = new MethodEntry(classNameWithDots, methodName,
+				descriptor);
+		updateFieldMethodList.add(entry);
+	}
+
+	public void addNotUpdatesFieldMethod(String className, String methodName,
+			String descriptor) {
+		String classNameWithDots = className.replace("/", ".");
+		MethodEntry entry = new MethodEntry(classNameWithDots, methodName,
+				descriptor);
+		notUpdateFieldMethodList.add(entry);
+	}
+
+	private final HashMap<MethodEntry, Set<MethodEntry>> staticCalls = new HashMap<MethodEntry, Set<MethodEntry>>();
+	private final HashMap<MethodEntry, Set<MethodEntry>> virtualCalls = new HashMap<MethodEntry, Set<MethodEntry>>();
+	private final HashMap<MethodEntry, Set<MethodEntry>> specialCalls = new HashMap<MethodEntry, Set<MethodEntry>>();
+	private final HashMap<MethodEntry, Set<MethodEntry>> interfaceCalls = new HashMap<MethodEntry, Set<MethodEntry>>();
+
+	public void addStaticCall(String sourceClassName, String sourceMethodName,
+			String sourceDescriptor, String targetClassName,
+			String targetMethodName, String targetDescriptor) {
+
+		MethodEntry sourceEntry = new MethodEntry(sourceClassName.replace("/",
+				"."), sourceMethodName, sourceDescriptor);
+		MethodEntry targetEntry = new MethodEntry(targetClassName.replace("/",
+				"."), targetMethodName, targetDescriptor);
+		if (!staticCalls.containsKey(sourceEntry)) {
+			staticCalls.put(sourceEntry, new HashSet<MethodEntry>());
+		}
+		staticCalls.get(sourceEntry).add(targetEntry);
+	}
+
+	public void addVirtualCall(String sourceClassName, String sourceMethodName,
+			String sourceDescriptor, String targetClassName,
+			String targetMethodName, String targetDescriptor) {
+
+		MethodEntry sourceEntry = new MethodEntry(sourceClassName.replace("/",
+				"."), sourceMethodName, sourceDescriptor);
+		MethodEntry targetEntry = new MethodEntry(targetClassName.replace("/",
+				"."), targetMethodName, targetDescriptor);
+		if (!virtualCalls.containsKey(sourceEntry)) {
+			virtualCalls.put(sourceEntry, new HashSet<MethodEntry>());
+		}
+		virtualCalls.get(sourceEntry).add(targetEntry);
+
+	}
+
+	public void addInterfaceCall(String sourceClassName,
+			String sourceMethodName, String sourceDescriptor,
+			String targetClassName, String targetMethodName,
+			String targetDescriptor) {
+
+		MethodEntry sourceEntry = new MethodEntry(sourceClassName.replace("/",
+				"."), sourceMethodName, sourceDescriptor);
+		MethodEntry targetEntry = new MethodEntry(targetClassName.replace("/",
+				"."), targetMethodName, targetDescriptor);
+		if (!interfaceCalls.containsKey(sourceEntry)) {
+			interfaceCalls.put(sourceEntry, new HashSet<MethodEntry>());
+		}
+		interfaceCalls.get(sourceEntry).add(targetEntry);
+
+	}
+
+	public void addSpecialCall(String sourceClassName, String sourceMethodName,
+			String sourceDescriptor, String targetClassName,
+			String targetMethodName, String targetDescriptor) {
+		MethodEntry sourceEntry = new MethodEntry(sourceClassName.replace("/",
+				"."), sourceMethodName, sourceDescriptor);
+		MethodEntry targetEntry = new MethodEntry(targetClassName.replace("/",
+				"."), targetMethodName, targetDescriptor);
+		if (!specialCalls.containsKey(sourceEntry)) {
+			specialCalls.put(sourceEntry, new HashSet<MethodEntry>());
+		}
+		specialCalls.get(sourceEntry).add(targetEntry);
+	}
+
+}
---
+++ src/main/java/org/evosuite/sandbox/Sandbox.java
@@ -0,0 +1,122 @@
+/**
+ * Copyright (C) 2011,2012 Gordon Fraser, Andrea Arcuri and EvoSuite
+ * contributors
+ * 
+ * This file is part of EvoSuite.
+ * 
+ * EvoSuite is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ * 
+ * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Public License for more details.
+ * 
+ * You should have received a copy of the GNU Public License along with
+ * EvoSuite. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.evosuite.sandbox;
+
+import org.evosuite.Properties;
+import org.evosuite.TestGenerationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Class which controls enabling and disabling sandbox.
+ * EvoSuite uses its own customized security manager.
+ * 
+ */
+public class Sandbox {
+
+	private static Logger logger = LoggerFactory.getLogger(Sandbox.class);
+
+	private static volatile MSecurityManager manager;
+
+	/**
+	 * count how often we tried to init the sandbox.
+	 * 
+	 * <p>
+	 * Ideally, the sandbox should be init only once.
+	 * Problem is, that we do compile and run the JUnit test cases (eg
+	 * to see if they compile with no problems, if their assertions 
+	 * are stable, etc), and those test cases do init/reset the sandbox
+	 */
+	private static volatile int counter;
+	
+	/**
+	 * Create and initialize security manager for SUT
+	 */
+	public static synchronized void initializeSecurityManagerForSUT() {
+		if (manager == null) {
+			manager = new MSecurityManager();
+			manager.makePriviligedAllCurrentThreads();
+			manager.apply();
+		} else {
+			logger.warn("Sandbox can be initalized only once");
+		}
+		
+		counter++;
+	}
+
+	public static void addPriviligedThread(Thread t) {
+		if (manager != null)
+			manager.addPrivilegedThread(t);
+	}
+
+	public static synchronized void resetDefaultSecurityManager() {
+		
+		counter--;
+		
+		if(counter==0){
+			if (manager != null) {
+				manager.restoreDefaultManager();
+			}
+			manager = null;
+		}
+	}
+
+	public static boolean isSecurityManagerInitialized() {
+		return manager != null;
+	}
+
+	public static void goingToExecuteSUTCode() {
+		if (!isSecurityManagerInitialized()) {
+			if(Properties.SANDBOX){
+				logger.error("Sandbox is not initialized!");
+			}
+			return;
+		}
+		manager.goingToExecuteTestCase();
+		PermissionStatistics.getInstance().getAndResetExceptionInfo();
+		
+		TestGenerationContext.getInstance().goingToExecuteSUTCode();
+	}
+
+	public static void doneWithExecutingSUTCode() {
+		if (!isSecurityManagerInitialized()) {
+			if(Properties.SANDBOX){
+				logger.error("Sandbox is not initialized!");
+			}
+			return;
+		}
+		manager.goingToEndTestCase();
+		TestGenerationContext.getInstance().doneWithExecuteingSUTCode();
+	}
+
+	public static void goingToExecuteUnsafeCodeOnSameThread() throws SecurityException,
+	        IllegalStateException {
+		if (!isSecurityManagerInitialized()) {
+			return;
+		}
+		manager.goingToExecuteUnsafeCodeOnSameThread();
+	}
+
+	public static void doneWithExecutingUnsafeCodeOnSameThread()
+	        throws SecurityException, IllegalStateException {
+		if (!isSecurityManagerInitialized()) {
+			return;
+		}
+		manager.doneWithExecutingUnsafeCodeOnSameThread();
+	}
+}
