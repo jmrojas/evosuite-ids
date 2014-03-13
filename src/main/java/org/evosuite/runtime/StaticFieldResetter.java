032809de5aa05336239ccd07367e6130b3bdca84
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2014-03-13 21:26:40 +0000

    Fix inner class naming if full package names are used

---
+++ src/main/java/org/evosuite/runtime/StaticFieldResetter.java
@@ -0,0 +1,55 @@
+package org.evosuite.runtime;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.evosuite.TestGenerationContext;
+import org.evosuite.agent.ToolsJarLocator;
+
+/**
+ * This class resets the static fields of a given class by invoking the <clinit> class initializer.
+ * In order to re-invoke the <clinit> this is duplicated with the method name "__STATIC_RESET".
+ * 
+ * @author galeotti
+ *
+ */
+public class StaticFieldResetter {
+
+	/**
+	 * The name of the instrumented duplication of the class initializer <clinit>
+	 */
+	private static final String STATIC_RESET = "__STATIC_RESET";
+
+	private static final StaticFieldResetter instance = new StaticFieldResetter();
+
+	public static StaticFieldResetter getInstance() {
+		return instance;
+	}
+
+	/**
+	 * Invoke the duplicated version of class initializar <clinit> 
+	 *  
+	 * @param classNameWithDots the class for invoking the duplicated version of class initializer <clinit>
+	 */
+	public void resetStaticFields(String classNameWithDots) {
+		try {
+			ClassLoader classLoader = TestGenerationContext.getInstance().getClassLoaderForSUT();
+			Class<?> clazz = classLoader.loadClass(classNameWithDots);
+			Method m = clazz.getMethod(STATIC_RESET, (Class<?>[]) null);
+			m.invoke(null, (Object[]) null);
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
