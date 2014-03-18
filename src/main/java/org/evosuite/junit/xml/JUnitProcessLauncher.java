4a75e86082705d06dbd69e4873b5b3dbec2f5e84
gordon <gordon@localhost>
2014-03-18 23:59:39 +0000

    Simplify dependency class to make test pass again

---
+++ src/main/java/org/evosuite/junit/xml/JUnitProcessLauncher.java
@@ -0,0 +1,149 @@
+package org.evosuite.junit.xml;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.evosuite.junit.JUnitExecutionException;
+import org.evosuite.junit.JUnitResult;
+import org.evosuite.utils.ClassPathHandler;
+import org.evosuite.utils.LoggingUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JUnitProcessLauncher {
+
+	private static final String JUNIT_ANALYZER_XML = "junitanalyzer.xml";
+
+	private static Logger logger = LoggerFactory
+			.getLogger(JUnitProcessLauncher.class);
+
+	private String[] parseCommand(String command) {
+		List<String> list = new ArrayList<String>();
+		for (String token : command.split(" ")) {
+			String entry = token.trim();
+			if (!entry.isEmpty()) {
+				list.add(entry);
+			}
+		}
+		String[] parsedCommand = list.toArray(new String[0]);
+		return parsedCommand;
+	}
+
+	public JUnitResult startNewJUnitProcess(Class<?>[] testClasses,
+			File testClassDir) throws JUnitExecutionException {
+		String baseDir = System.getProperty("user.dir");
+		File dir = new File(baseDir);
+		String xmlFileName = baseDir + File.separatorChar + JUNIT_ANALYZER_XML;
+
+		String junitClassPath;
+		if (testClassDir != null) {
+			junitClassPath = testClassDir.getAbsolutePath()
+					+ File.pathSeparatorChar;
+		} else {
+			junitClassPath = "";
+		}
+		junitClassPath += ClassPathHandler.getInstance().getEvoSuiteClassPath();
+		junitClassPath += File.pathSeparatorChar;
+		junitClassPath += ClassPathHandler.getInstance()
+				.getTargetProjectClasspath();
+
+		String command = "java";
+		command += " -cp " + junitClassPath;
+		command += " " + JUnitXmlDocMain.class.getCanonicalName();
+		for (Class<?> testClass : testClasses) {
+			command += " " + testClass.getCanonicalName();
+		}
+
+		logger.debug("Checking XML file already exists " + xmlFileName);
+		File xmlFile = new File(xmlFileName);
+		if (xmlFile.exists()) {
+			logger.debug("Deleting XML file " + xmlFileName);
+			xmlFile.delete();
+		}
+
+		command += " " + xmlFileName;
+
+		String[] parsedCommand = parseCommand(command);
+
+		ProcessBuilder builder = new ProcessBuilder(parsedCommand);
+		builder.directory(dir);
+		builder.redirectErrorStream(true);
+
+		LoggingUtils.getEvoLogger().info(
+				"Going to start process for running JUnit on : " + command);
+		logger.debug("Base directory: " + baseDir);
+		logger.debug("Command: " + command);
+
+		try {
+			Process process = builder.start();
+
+			InputStream stdout = process.getInputStream();
+			logger.debug("JUnit process output:");
+
+			do {
+				readInputStream("Finished JUnit process output - ", stdout);
+			} while (!isFinished(process));
+
+			int exitValue = process.exitValue();
+			LoggingUtils.getEvoLogger().info("JUnit process finished");
+			logger.debug("JUnit process exit code was " + exitValue);
+			if (exitValue != 0) {
+				logger.warn("JUnit process XML did not finish correctly. Exit code: "
+						+ exitValue);
+				throw new JUnitExecutionException(
+						"Execution of java command did not end correctly: "
+								+ command);
+			}
+
+			if (xmlFile.exists()) {
+				logger.debug("Reading JUnitResult from file: " + xmlFileName);
+				JUnitXmlResultProxy proxy = new JUnitXmlResultProxy();
+				JUnitResult result = proxy.readFromXmlFile(xmlFileName);
+				xmlFile.delete();
+				LoggingUtils.getEvoLogger().info(
+						"JUnit finished correctly and created JUnit result.");
+				return result;
+			} else {
+				logger.warn("JUnit process XML file does not exists: "
+						+ xmlFile.getAbsolutePath());
+				logger.debug("XML file was expected because JUnit process finished correctly with exit code was "
+						+ exitValue + " ");
+				throw new JUnitExecutionException(
+						"Expected result of JUnitXmlProxy was not found "
+								+ xmlFileName);
+			}
+
+		} catch (IOException e) {
+			logger.warn("IOException during JUnit process execution ");
+			throw new JUnitExecutionException(e);
+		} catch (JUnitXmlResultProxyException e) {
+			logger.warn("JUnitXmlResultProxyException during JUnit process execution ");
+			throw new JUnitExecutionException(e);
+		}
+	}
+
+	private boolean isFinished(Process process) {
+		try {
+			process.exitValue();
+			return true;
+		} catch (IllegalThreadStateException ex) {
+			return false;
+		}
+	}
+
+	private void readInputStream(String prefix, InputStream in)
+			throws IOException {
+		InputStreamReader is = new InputStreamReader(in);
+		BufferedReader br = new BufferedReader(is);
+		String read = br.readLine();
+		while (read != null) {
+			logger.debug(prefix + read);
+			read = br.readLine();
+		}
+	}
+}
