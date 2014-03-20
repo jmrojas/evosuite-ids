12149c09ce9a9ef38f3337c429824389a4a8b54a
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2014-03-20 08:16:31 +0000

    Make LogRecord sequence number deterministic (0)

---
+++ src/test/java/com/examples/with/different/packagename/test/ExampleObserverClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.examples.with.different.packagename.test;
+
+/**
+ * @author fraser
+ * 
+ */
+public class ExampleObserverClass {
+	private int privateMember = 0;
+
+	public void setMember(int x) {
+		privateMember = x;
+	}
+
+	public int getMember() {
+		return privateMember;
+	}
+}
