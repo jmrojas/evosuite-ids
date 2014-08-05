04a28fc97a254ba6026a4cf02d7fba22fd74001c
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2014-08-05 07:28:16 +0100

    Add more generics tests

---
+++ runtime/src/test/java/org/evosuite/runtime/agent/InstrumentingAgent_IT.java
@@ -0,0 +1,134 @@
+package org.evosuite.runtime.agent;
+
+import java.lang.instrument.Instrumentation;
+
+import org.junit.*;
+
+import org.evosuite.runtime.RuntimeSettings;
+import org.evosuite.runtime.agent.InstrumentingAgent;
+
+import com.examples.with.different.packagename.agent.AbstractTime;
+import com.examples.with.different.packagename.agent.ConcreteTime;
+import com.examples.with.different.packagename.agent.ExtendingTimeC;
+import com.examples.with.different.packagename.agent.TimeA;
+import com.examples.with.different.packagename.agent.TimeB;
+import com.examples.with.different.packagename.agent.TimeC;
+
+/**
+ * Note: this needs be run as an integration test (IT), as it requires
+ * the creation of the jar file first.
+ * This is automatically set up in the pom file, but the test might fail
+ * if run directly from an IDE
+ * 
+ * @author arcuri
+ *
+ */
+public class InstrumentingAgent_IT {
+
+	private final boolean replaceCalls = RuntimeSettings.mockJVMNonDeterminism;
+
+	@BeforeClass
+	public static void initClass(){
+		InstrumentingAgent.initialize();
+	}
+	
+	@Before
+	public void storeValues() {
+		RuntimeSettings.mockJVMNonDeterminism = true;
+	}
+
+	@After
+	public void resetValues() {
+		RuntimeSettings.mockJVMNonDeterminism = replaceCalls;
+	}
+
+	@Test
+	public void testTime(){
+
+		long now = System.currentTimeMillis();
+		Assert.assertTrue("",TimeB.getTime() >= now);
+		
+		long expected = 42;
+		org.evosuite.runtime.System.setCurrentTimeMillis(expected);
+
+		try{
+			InstrumentingAgent.activate();
+			Assert.assertEquals(expected, TimeA.getTime());
+		} finally {
+			InstrumentingAgent.deactivate();
+		}
+	}
+	
+	@Test
+	public void testTransformationInAbstractClass(){
+		long expected = 42;
+		org.evosuite.runtime.System.setCurrentTimeMillis(expected);
+		try{
+			InstrumentingAgent.activate();
+			AbstractTime time = new ConcreteTime();
+			Assert.assertEquals(expected, time.getTime());
+		} finally {
+			InstrumentingAgent.deactivate();
+		}
+	}
+
+	@Test
+	public void testTransformationInClassExtendingAbstract(){
+		long expected = 42;
+		org.evosuite.runtime.System.setCurrentTimeMillis(expected);
+		try{
+			InstrumentingAgent.activate();
+			ConcreteTime time = new ConcreteTime();
+			Assert.assertEquals(expected, time.getTime());
+		} finally {
+			InstrumentingAgent.deactivate();
+		}
+	}
+
+	@Test
+	public void testTransformation(){
+		long expected = 42;
+		org.evosuite.runtime.System.setCurrentTimeMillis(expected);
+		try{
+			InstrumentingAgent.activate();
+			TimeC time = new TimeC();
+			Assert.assertEquals(expected, time.getTime());
+		} finally {
+			InstrumentingAgent.deactivate();
+		}
+	}
+
+	@Test
+	public void testTransformationInExtendingClass(){
+		long expected = 42;
+		org.evosuite.runtime.System.setCurrentTimeMillis(expected);
+		try{
+			InstrumentingAgent.activate();
+			ExtendingTimeC time = new ExtendingTimeC();
+			Assert.assertEquals(expected, time.getTime());
+		} finally {
+			InstrumentingAgent.deactivate();
+		}
+	}
+
+	@Test
+	public void testInstrumetation() throws Exception{
+		try{
+			InstrumentingAgent.activate();
+			
+			Instrumentation inst = InstrumentingAgent.getInstumentation();
+			Assert.assertNotNull(inst);
+			ClassLoader loader = this.getClass().getClassLoader();
+			Assert.assertTrue(inst.isModifiableClass(loader.loadClass(TimeA.class.getName())));
+			Assert.assertTrue(inst.isModifiableClass(loader.loadClass(TimeB.class.getName())));
+			Assert.assertTrue(inst.isModifiableClass(loader.loadClass(TimeC.class.getName())));
+			Assert.assertTrue(inst.isModifiableClass(loader.loadClass(ExtendingTimeC.class.getName())));
+			Assert.assertTrue(inst.isModifiableClass(loader.loadClass(ConcreteTime.class.getName())));
+			Assert.assertTrue(inst.isModifiableClass(loader.loadClass(AbstractTime.class.getName())));
+			
+		} finally{
+			InstrumentingAgent.deactivate();
+		}
+	}
+}
+
