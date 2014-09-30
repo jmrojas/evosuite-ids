94315eabec3d5294bb51cc9f9b04f22de258cd2f
Jose Rojas <jmrs84@email.com>
2014-09-30 15:58:22 +0100

    remove file committed by mistake

---
+++ master/src/test/java/org/evosuite/assertion/stable/TestHasClinit.java
@@ -0,0 +1,76 @@
+package org.evosuite.assertion.stable;
+
+import java.util.Map;
+
+import org.evosuite.EvoSuite;
+import org.evosuite.Properties;
+import org.evosuite.SystemTest;
+import org.evosuite.ga.metaheuristics.GeneticAlgorithm;
+import org.evosuite.statistics.OutputVariable;
+import org.evosuite.statistics.RuntimeVariable;
+import org.evosuite.statistics.backend.DebugStatisticsBackend;
+import org.evosuite.testsuite.TestSuiteChromosome;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.examples.with.different.packagename.stable.HasClinit;
+
+public class TestHasClinit extends SystemTest {
+	private final boolean DEFAULT_RESET_STATIC_FIELDS = Properties.RESET_STATIC_FIELDS;
+	private final boolean DEFAULT_JUNIT_CHECK_ON_SEPARATE_PROCESS = Properties.JUNIT_CHECK_ON_SEPARATE_PROCESS;
+	private final boolean DEFAULT_REPLACE_CALLS = Properties.REPLACE_CALLS;
+	private final boolean DEFAULT_JUNIT_CHECK = Properties.JUNIT_CHECK;
+	private final boolean DEFAULT_JUNIT_TESTS = Properties.JUNIT_TESTS;
+	private final boolean DEFAULT_PURE_INSPECTORS = Properties.PURE_INSPECTORS;
+	private final boolean DEFAULT_SANDBOX = Properties.SANDBOX;
+	
+
+	@Before
+	public void configureProperties() {
+		Properties.RESET_STATIC_FIELDS = true;
+		Properties.SANDBOX = true;
+		Properties.REPLACE_CALLS = true;
+		Properties.JUNIT_CHECK = true;
+		Properties.JUNIT_TESTS = true;
+		Properties.PURE_INSPECTORS = true;
+		Properties.JUNIT_CHECK_ON_SEPARATE_PROCESS = false;
+
+	}
+
+	@After
+	public void restoreProperties() {
+		Properties.RESET_STATIC_FIELDS = DEFAULT_RESET_STATIC_FIELDS;
+		Properties.JUNIT_CHECK_ON_SEPARATE_PROCESS = DEFAULT_JUNIT_CHECK_ON_SEPARATE_PROCESS;
+		Properties.SANDBOX = DEFAULT_SANDBOX;
+		Properties.REPLACE_CALLS = DEFAULT_REPLACE_CALLS;
+		Properties.JUNIT_CHECK = DEFAULT_JUNIT_CHECK;
+		Properties.JUNIT_TESTS = DEFAULT_JUNIT_TESTS;
+		Properties.PURE_INSPECTORS = DEFAULT_PURE_INSPECTORS;
+	}
+
+	@Test
+	public void testNoClinit() {
+		EvoSuite evosuite = new EvoSuite();
+
+		String targetClass = HasClinit.class.getCanonicalName();
+		Properties.TARGET_CLASS = targetClass;
+		Properties.OUTPUT_VARIABLES=""+RuntimeVariable.HadUnstableTests;
+		String[] command = new String[] { "-generateSuite", "-class",
+				targetClass };
+
+		Object result = evosuite.parseCommandLine(command);
+
+		GeneticAlgorithm<?> ga = getGAFromResult(result);
+		TestSuiteChromosome best = (TestSuiteChromosome) ga.getBestIndividual();
+		System.out.println("EvolvedTestSuite:\n" + best);
+
+		Map<String, OutputVariable<?>> map = DebugStatisticsBackend.getLatestWritten();
+		Assert.assertNotNull(map);
+		OutputVariable unstable = map.get(RuntimeVariable.HadUnstableTests.toString());
+		Assert.assertNotNull(unstable);
+		Assert.assertEquals(Boolean.FALSE, unstable.getValue());
+	}
+
+}
