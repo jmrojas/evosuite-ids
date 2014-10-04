c1f6be6cf784d57902c3e15427394656e9f0cdb7
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2014-10-04 14:24:02 +0100

    Fix test that was dependent on specific random seed

---
+++ runtime/src/main/java/org/evosuite/runtime/RuntimeSettings.java
@@ -0,0 +1,48 @@
+package org.evosuite.runtime;
+
+import org.evosuite.runtime.sandbox.Sandbox;
+
+/**
+ * Created by arcuri on 6/10/14.
+ */
+public class RuntimeSettings {
+
+    /*
+     * By default, all these properties should be false.
+     *
+     * WARNING If this behavior is changed, or any variable name is changed,
+     * we HAVE TO update how JUnit code is generated
+     */
+
+    /**
+     * Shall the test cases use the mocking framework to remove non-determinism like
+     * CPU clock?
+     */
+    public static boolean mockJVMNonDeterminism = false;
+
+    /**
+     * Should the use of System.in be mocked?
+     */
+    public static boolean mockSystemIn = false;
+
+    /**
+     * Shall the test cases use a virtual file system?
+     */
+    public static boolean useVFS = false;
+
+    /**
+     * Should the static state be reset after each test execution?
+     */
+    public static boolean resetStaticState = false;
+
+    /**
+     * How is the sandbox configured?
+     */
+    public static Sandbox.SandboxMode sandboxMode = Sandbox.SandboxMode.RECOMMENDED;
+
+    /**
+     * How many threads is each test allowed to start?
+     * Note: such checks depend on RuntimeSettings#mockJVMNonDeterminism
+     */
+    public static int maxNumberOfThreads = 100;
+}
