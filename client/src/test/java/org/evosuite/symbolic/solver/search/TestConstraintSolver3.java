2ac966c4fb457f902cac881d197b5eaf35d70120
Andrea Arcuri <arcuri82@gmail.com>
2014-10-18 11:31:37 +0200

    adding stop option to IntelliJ plugin

---
+++ client/src/test/java/org/evosuite/symbolic/solver/search/TestConstraintSolver3.java
@@ -0,0 +1,75 @@
+package org.evosuite.symbolic.solver.search;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
+
+import org.evosuite.Properties;
+import org.evosuite.Properties.LocalSearchBudgetType;
+import org.evosuite.symbolic.expr.Comparator;
+import org.evosuite.symbolic.expr.Constraint;
+import org.evosuite.symbolic.expr.IntegerConstraint;
+import org.evosuite.symbolic.expr.bv.IntegerConstant;
+import org.evosuite.symbolic.expr.bv.StringToIntegerCast;
+import org.evosuite.symbolic.expr.str.StringVariable;
+import org.evosuite.symbolic.solver.ConstraintSolverTimeoutException;
+import org.evosuite.symbolic.solver.search.EvoSuiteSolver;
+import org.junit.Test;
+
+public class TestConstraintSolver3 {
+
+	private static final String INIT_STRING = "125";
+	private static final int EXPECTED_INTEGER = 126;
+
+	private static Collection<Constraint<?>> buildConstraintSystem() {
+
+		StringVariable var0 = new StringVariable("var0", INIT_STRING);
+
+		StringToIntegerCast castStr = new StringToIntegerCast(var0,
+				(long) Integer.parseInt(INIT_STRING));
+
+		IntegerConstant const126 = new IntegerConstant(EXPECTED_INTEGER);
+
+		IntegerConstraint constr1 = new IntegerConstraint(castStr,
+				Comparator.EQ, const126);
+
+		return Arrays.<Constraint<?>> asList(constr1);
+	}
+
+	@Test
+	public void test() {
+		Properties.LOCAL_SEARCH_BUDGET = 100; // 5000000000000L; TODO - ??
+		Properties.LOCAL_SEARCH_BUDGET_TYPE = LocalSearchBudgetType.FITNESS_EVALUATIONS;
+
+		Collection<Constraint<?>> constraints = buildConstraintSystem();
+
+		System.out.println("Constraints:");
+		for (Constraint<?> c : constraints) {
+			System.out.println(c.toString());
+		}
+
+		System.out.println("");
+		System.out.println("Initial: " + INIT_STRING);
+
+		EvoSuiteSolver seeker = new EvoSuiteSolver();
+		Map<String, Object> model;
+		try {
+			model = seeker.solve(constraints);
+			if (model == null) {
+				fail("search was unsuccessfull");
+			} else {
+				Object var0 = model.get("var0");
+				System.out.println("Expected: " + EXPECTED_INTEGER);
+				System.out.println("Found: " + var0);
+
+				assertEquals(String.valueOf(EXPECTED_INTEGER), var0);
+			}
+		} catch (ConstraintSolverTimeoutException e) {
+			fail();
+		}
+
+	}
+}
