e4ec1dc9a945ea462f6de1b4fb09d5cc1e1c9140
Jose Rojas <jmrs84@email.com>
2015-03-23 18:55:03 +0000

    archive mutation runtime var

---
+++ client/src/main/java/org/evosuite/rmi/service/ClientNodeLocal.java
@@ -0,0 +1,26 @@
+package org.evosuite.rmi.service;
+
+import org.evosuite.ga.Chromosome;
+import org.evosuite.statistics.RuntimeVariable;
+
+/**
+ * Client Node view in the client process.
+ * @author arcuri
+ *
+ */
+public interface ClientNodeLocal {
+
+	public boolean init();
+
+	public void trackOutputVariable(RuntimeVariable variable, Object value);
+	
+    public void publishPermissionStatistics();
+
+	public void changeState(ClientState state);
+
+	public void changeState(ClientState state, ClientStateInformation information);
+
+	public void updateStatistics(Chromosome individual);
+
+	public void waitUntilDone();
+}
