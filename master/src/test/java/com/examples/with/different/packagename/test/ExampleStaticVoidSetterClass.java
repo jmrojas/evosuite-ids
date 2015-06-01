6da6f9e832dd230859f04e44a8d773bd20aa7e1b
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2015-06-01 10:42:07 +0100

    Do not replace invokespecial with static mocks, unless they are constructors

---
+++ master/src/test/java/com/examples/with/different/packagename/test/ExampleStaticVoidSetterClass.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.examples.with.different.packagename.test;
+
+/**
+ * @author fraser
+ * 
+ */
+public class ExampleStaticVoidSetterClass {
+
+	public static void setExampleMember(ExampleObserverClass x, int y) {
+		x.setMember(y);
+	}
+}
