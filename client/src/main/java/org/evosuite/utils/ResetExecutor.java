1aaca0ca7d4ad910da2752eb3be5905a9603cb23
arcuri82 <arcuri82@localhost>
2015-06-10 18:07:19 +0200

    fixed bug in which target jar ended up at the end of the CP

---
+++ client/src/main/java/org/evosuite/utils/ResetExecutor.java
@@ -0,0 +1,85 @@
+package org.evosuite.utils;
+
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.evosuite.Properties;
+import org.evosuite.TestGenerationContext;
+import org.evosuite.TimeController;
+import org.evosuite.coverage.mutation.MutationObserver;
+import org.evosuite.runtime.Runtime;
+import org.evosuite.runtime.classhandling.ClassResetter;
+import org.evosuite.runtime.classhandling.ResetManager;
+import org.evosuite.runtime.sandbox.Sandbox;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ResetExecutor {
+
+	private final static Logger logger = LoggerFactory.getLogger(ResetExecutor.class);
+
+	private static final ResetExecutor instance = new ResetExecutor();
+
+	private ResetExecutor() {
+	}
+	
+
+	public synchronized static ResetExecutor getInstance() {
+		return instance;
+	}
+
+	public void resetAllClasses() {
+		List<String> classesToReset = ResetManager.getInstance().getClassResetOrder();
+		resetClasses(classesToReset);
+	}
+
+	public void resetClasses(List<String> classesToReset) {
+		//try to reset each collected class
+
+		ClassResetter.getInstance().setClassLoader(TestGenerationContext.getInstance().getClassLoaderForSUT());
+
+		long start = System.currentTimeMillis();
+
+		for (String className : classesToReset) {
+			//this can be expensive
+
+			long elapsed = System.currentTimeMillis() - start;
+
+			if(! TimeController.getInstance().isThereStillTimeInThisPhase() || elapsed > Properties.TIMEOUT_RESET){
+				logger.warn("Stopped resetting of classes due to timeout");
+				break;
+			}
+			resetClass(className);
+		}
+	}
+
+	private void resetClass(String className) {
+
+		//className.__STATIC_RESET() exists
+		logger.debug("Resetting class " + className);
+		
+		int mutationActive = MutationObserver.activeMutation;
+		MutationObserver.deactivateMutation();
+
+		//execute __STATIC_RESET()
+		Sandbox.goingToExecuteSUTCode();
+        TestGenerationContext.getInstance().goingToExecuteSUTCode();
+
+		Runtime.getInstance().resetRuntime(); //it is important to initialize the VFS
+
+		try {
+			Method resetMethod = ClassResetter.getInstance().getResetMethod(className);
+			if (resetMethod!=null) {
+				resetMethod.invoke(null, (Object[]) null);
+			}
+		} catch (Throwable  e) {
+			ClassResetter.getInstance().logWarn(className, e.getClass() + " thrown during execution of method  __STATIC_RESET() for class " + className + ", " + e.getCause());
+		}  finally {
+			Sandbox.doneWithExecutingSUTCode();
+            TestGenerationContext.getInstance().doneWithExecuteingSUTCode();
+			MutationObserver.activateMutation(mutationActive);
+		}
+	}
+}
---
+++ master/src/main/java/org/evosuite/continuous/project/ProjectAnalyzer.java
@@ -0,0 +1,235 @@
+package org.evosuite.continuous.project;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import org.evosuite.Properties;
+import org.evosuite.Properties.AvailableSchedule;
+import org.evosuite.classpath.ClassPathHandler;
+import org.evosuite.classpath.ResourceList;
+import org.evosuite.continuous.job.schedule.HistorySchedule;
+import org.evosuite.continuous.project.ProjectStaticData.ClassInfo;
+import org.evosuite.coverage.branch.BranchPool;
+import org.evosuite.instrumentation.InstrumentingClassLoader;
+import org.evosuite.junit.CoverageAnalysis;
+import org.evosuite.runtime.sandbox.Sandbox;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * <p>
+ * This class is used to analyze and gather all the
+ * static information of the target project.
+ * </p>
+ * 
+ * <p>
+ * To be useful, this analysis does not need to be 100% precise,
+ * as we use the generated statistics <i>only</i> for heuristics
+ * </p>
+ * 
+ * <p>
+ * Note: this class assumes the classpath is properly set
+ * 
+ * @author arcuri
+ *
+ */
+public class ProjectAnalyzer {
+
+	private static Logger logger = LoggerFactory.getLogger(ProjectAnalyzer.class);
+
+	/**
+	 * the folder/jar where to find the .class files used as CUTs
+	 */
+	private final String target;
+
+	/**
+	 * package prefix to select a subset of classes on classpath/target to define
+	 * what to run CTG on
+	 */
+	private final String prefix;
+
+	private transient Set<String> cutsToAnalyze;
+	
+	/**
+	 * When specifying a set of CUTs, still check if they do exist (eg scan folder to search for
+	 * them), instead of justing using them directly (and get errors later)
+	 * 
+	 */
+	private boolean validateCutsToAnalyze;
+
+	/**
+	 * Main constructor
+	 * 
+	 * @param target
+	 * @param prefix
+	 * @param cuts
+	 */
+	public ProjectAnalyzer(String target, String prefix, String[] cuts) {
+		super();
+		this.target = target;
+		this.prefix = prefix==null ? "" : prefix;
+		this.validateCutsToAnalyze = true;
+
+		if(cuts == null){
+			this.cutsToAnalyze = null;			
+		} else {
+			this.cutsToAnalyze = new LinkedHashSet<>();
+			for(String s : cuts){
+				if(s!=null && !s.isEmpty()){
+					cutsToAnalyze.add(s.trim());
+				}
+			}
+		}
+	}
+
+	/**
+	 * Instead of scanning for classes in the given target, directly specify
+	 * the class names the project is composed by
+	 * 
+	 * <p>
+	 * Note: this constructor is mainly meant for unit tests
+	 * @param cuts
+	 */
+	public ProjectAnalyzer(String[] cuts) throws NullPointerException {
+		super();
+		if(cuts==null){
+			throw new NullPointerException("Input array cannot be null");
+		}
+		this.target = null;
+		this.prefix = null;
+		this.validateCutsToAnalyze = false;
+		this.cutsToAnalyze = new LinkedHashSet<>();		
+		cutsToAnalyze.addAll(Arrays.asList(cuts));
+	}
+
+	private Collection<String> getCutsToAnalyze(){
+
+		if(cutsToAnalyze!=null && !validateCutsToAnalyze){
+			// this is mainly in test cases
+			return cutsToAnalyze;
+		}
+
+		Set<String> suts = null;
+
+		if(target!=null){
+			if(!target.contains(File.pathSeparator)){
+				suts = ResourceList.getAllClasses(target, prefix, false);
+			} else {
+				suts = new LinkedHashSet<>();
+				for(String element : target.split(File.pathSeparator)){
+					suts.addAll(ResourceList.getAllClasses(element, prefix, false));
+				}
+			}
+		} else {
+			/*
+			 * if no target specified, just grab everything on SUT classpath
+			 */
+			suts = ResourceList.getAllClasses(ClassPathHandler.getInstance().getTargetProjectClasspath(), prefix, false);
+		}
+
+		List<String> cuts = new LinkedList<String>();
+
+		for (String className : suts) {
+			
+			if(cutsToAnalyze!=null && !cutsToAnalyze.contains(className)){
+				/*
+				 * Note: if this is happens, it is not necessarily an error.
+				 * For example, this will happen when CTG is run on a multi-module
+				 * maven project 
+				 */
+				continue;
+			}
+			
+			try {
+				Class<?> clazz = Class.forName(className);
+				if (!CoverageAnalysis.isTest(clazz)){
+					cuts.add(className);
+				}
+			} catch (ClassNotFoundException e) {
+				logger.error(""+e,e);
+			} catch(ExceptionInInitializerError | NoClassDefFoundError e){
+                /**
+                 * TODO: for now we skip it, but at a certain point
+                 * we should able to handle it, especially if it
+                 * is due to static state initialization
+                 */
+                logger.warn("Cannot initialize class: "+className);
+            }
+
+		}
+		return cuts;
+
+	}
+
+	/**
+	 * Analyze the classes in the given target
+	 * @return
+	 */
+	public ProjectStaticData analyze(){		
+
+		ProjectStaticData data = new ProjectStaticData();
+		if(Properties.CTG_SCHEDULE.equals(AvailableSchedule.HISTORY)){
+			data.initializeLocalHistory();
+		}
+		
+		for (String className : getCutsToAnalyze()) {
+			Class<?> theClass = null; 
+			int numberOfBranches = -1;			
+			boolean hasCode = false;
+
+			Properties.TARGET_CLASS = className;
+			InstrumentingClassLoader instrumenting = new InstrumentingClassLoader();
+
+			BranchPool.reset();
+
+			try{
+				/*
+				 * to access number of branches, we need to use
+				 * instrumenting class loader. But loading a class would
+				 * execute its static code, and so we need to 
+				 * use a security manager. 
+				 */
+				Sandbox.goingToExecuteUnsafeCodeOnSameThread();
+				instrumenting.loadClass(className);
+
+				numberOfBranches = BranchPool.getBranchCounter();
+				hasCode = (numberOfBranches > 0) || (BranchPool.getBranchlessMethods().size() > 0);
+
+				/*
+				 * just to avoid possible issues with instrumenting classloader
+				 */
+				theClass = ClassLoader.getSystemClassLoader().loadClass(className);
+
+				//TODO kind
+				//if(theClass.isInterface()){
+				//	kind = ClassKind.INTERFACE;
+				//} else if(theClass.is  Modifier.isAbstract( someClass.getModifiers() );
+
+			} catch  (Exception e) {
+				logger.warn("Cannot handle "+className+" due to: "+e.getClass()+" "+e.getMessage());
+				continue;
+			}
+			finally {
+				Sandbox.doneWithExecutingUnsafeCodeOnSameThread();
+				BranchPool.reset();
+				Properties.TARGET_CLASS = "";
+			}
+
+			ClassInfo ci = new ClassInfo(theClass, numberOfBranches, hasCode);
+			data.addNewClass(ci);
+
+			if (Properties.CTG_SCHEDULE == AvailableSchedule.HISTORY) {
+				ci.setChanged(data.hasChanged(theClass.getCanonicalName() + ".java"));
+				ci.isToTest(data.isToTest(theClass.getCanonicalName(), HistorySchedule.COMMIT_IMPROVEMENT));
+			}
+		}
+
+		return data;
+	}
+}
+
