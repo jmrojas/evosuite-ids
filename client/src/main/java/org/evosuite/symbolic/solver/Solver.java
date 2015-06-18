cd225c478f0f50503e730130ec9a31b04dffcece
Juan Galeotti <galeotti@cs.uni-saarland.de>
2015-06-18 13:58:40 -0300

    more SmtExpr evaluation

---
+++ client/src/main/java/org/evosuite/symbolic/solver/Solver.java
@@ -0,0 +1,180 @@
+/**
+ * Copyright (C) 2011,2012 Gordon Fraser, Andrea Arcuri and EvoSuite
+ * contributors
+ *
+ * This file is part of EvoSuite.
+ *
+ * EvoSuite is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Public License for more details.
+ *
+ * You should have received a copy of the GNU Public License along with
+ * EvoSuite. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.evosuite.symbolic.solver;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.evosuite.symbolic.expr.Constraint;
+import org.evosuite.symbolic.expr.Variable;
+import org.evosuite.symbolic.expr.bv.IntegerVariable;
+import org.evosuite.symbolic.expr.fp.RealVariable;
+import org.evosuite.symbolic.expr.str.StringVariable;
+import org.evosuite.symbolic.solver.smt.SmtExpr;
+import org.evosuite.symbolic.solver.smt.SmtExprEvaluator;
+import org.evosuite.symbolic.solver.smt.SmtExprPrinter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Interface for SMT solvers
+ * 
+ * @author Gordon Fraser
+ */
+public abstract class Solver {
+
+	private final boolean addMissingVariables;
+
+	protected boolean addMissingVariables() {
+		return addMissingVariables;
+	}
+
+	public Solver() {
+		addMissingVariables = false;
+	}
+
+	public Solver(boolean addMissingVariables) {
+		this.addMissingVariables = addMissingVariables;
+	}
+
+	static Logger logger = LoggerFactory.getLogger(Solver.class);
+
+	/**
+	 * Get concrete values for the parameters used in the path conditions.
+	 * 
+	 * @return A {@link Map} where the name of the parameter is the key and the
+	 *         concrete value that the solver used is the object.
+	 * @param constraints
+	 *            a {@link java.util.Collection} object.
+	 */
+	public abstract Map<String, Object> solve(
+			Collection<Constraint<?>> constraints)
+			throws ConstraintSolverTimeoutException;
+
+	/**
+	 * Returns a mapping from variables to their current concrete values.
+	 * 
+	 * @param variables
+	 * @return a mapping from variables to their current concrete values.
+	 */
+	protected static Map<String, Object> getConcreteValues(
+			Set<Variable<?>> variables) {
+
+		Map<String, Object> concrete_values = new HashMap<String, Object>();
+		for (Variable<?> v : variables) {
+			String var_name = v.getName();
+			Object concrete_value = v.getConcreteValue();
+			concrete_values.put(var_name, concrete_value);
+		}
+		return concrete_values;
+	}
+
+	/**
+	 * Creates a set with all the variables in the constraints.
+	 * 
+	 * @param constraints
+	 *            the constraint system
+	 * @return the set of variables in the constraint system
+	 */
+	protected static Set<Variable<?>> getVariables(
+			Collection<Constraint<?>> constraints) {
+		Set<Variable<?>> variables = new HashSet<Variable<?>>();
+		for (Constraint<?> c : constraints) {
+			variables.addAll(c.getLeftOperand().getVariables());
+			variables.addAll(c.getRightOperand().getVariables());
+		}
+		return variables;
+	}
+
+	/**
+	 * Restore all concrete values of the variables using the concrete_values
+	 * mapping.
+	 * 
+	 * @param variables
+	 * @param concrete_values
+	 */
+	protected static void setConcreteValues(Set<Variable<?>> variables,
+			Map<String, Object> concrete_values) {
+		for (Variable<?> v : variables) {
+
+			String var_name = v.getName();
+
+			if (!concrete_values.containsKey(var_name)) {
+				continue;
+			}
+
+			Object concreteValue = concrete_values.get(var_name);
+
+			if (v instanceof StringVariable) {
+				StringVariable sv = (StringVariable) v;
+				String concreteString = (String) concreteValue;
+				sv.setConcreteValue(concreteString);
+			} else if (v instanceof IntegerVariable) {
+				IntegerVariable iv = (IntegerVariable) v;
+				Long concreteInteger = (Long) concreteValue;
+				iv.setConcreteValue(concreteInteger);
+			} else if (v instanceof RealVariable) {
+				RealVariable ir = (RealVariable) v;
+				Double concreteReal = (Double) concreteValue;
+				ir.setConcreteValue(concreteReal);
+			} else {
+				logger.warn("unknow variable type " + v.getClass().getName());
+			}
+		}
+	}
+
+	private static final double DELTA = 1e-15;
+
+	protected static boolean checkSolution(List<SmtExpr> smtExpressions,
+			Map<String, Object> solution) {
+		
+		SmtExprEvaluator evaluator = new SmtExprEvaluator(solution);
+		for (SmtExpr smtExpr : smtExpressions) {
+			Boolean ret_val = (Boolean) smtExpr.accept(evaluator, null);
+			if (ret_val.booleanValue() == false) {
+				logger.debug("The following SMT expression was not satisfied by the given solution");
+				String smtExprStr = smtExpr.accept(new SmtExprPrinter(), null);
+				logger.debug(smtExprStr);
+				return false;
+			}
+		}
+
+		logger.debug("All SMT expressions were satisfied with the given solution");
+		return true;
+	}
+
+	protected static boolean checkSolution(
+			Collection<Constraint<?>> constraints, Map<String, Object> solution) {
+
+		Set<Variable<?>> variables = getVariables(constraints);
+		Map<String, Object> initialValues = getConcreteValues(variables);
+		setConcreteValues(variables, solution);
+		double distance = DistanceEstimator.getDistance(constraints);
+		setConcreteValues(variables, initialValues);
+		if (distance <= DELTA) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
---
+++ runtime/src/main/java/org/evosuite/runtime/instrumentation/RuntimeInstrumentation.java
@@ -0,0 +1,150 @@
+package org.evosuite.runtime.instrumentation;
+
+import java.util.List;
+
+import org.evosuite.runtime.RuntimeSettings;
+import org.evosuite.runtime.util.ComputeClassWriter;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.tree.ClassNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class is responsible for the bytecode instrumentation
+ * needed for the generated JUnit test cases.
+ *
+ * <p>
+ * Note: the instrumentation will be part of the final JUnit tests and, as such, we should
+ * only keep the instrumentation that affect the functional behavior (so, no branch distances, etc).
+ *
+ * Created by arcuri on 6/11/14.
+ */
+public class RuntimeInstrumentation {
+
+    private static Logger logger = LoggerFactory.getLogger(RuntimeInstrumentation.class);
+
+    /**
+     * If we are re-instrumenting a class, then we cannot change its
+     * signature: eg add new methods
+     *
+	 * TODO: remove once we fix instrumentation
+	 */
+	private volatile boolean retransformingMode;
+
+	public RuntimeInstrumentation(){
+		retransformingMode = false;
+	}
+
+	public void setRetransformingMode(boolean on){
+		retransformingMode = on;
+	}
+
+    public static boolean checkIfCanInstrument(String className) {
+        for (String s : RuntimeInstrumentation.getPackagesShouldNotBeInstrumented()) {
+            if (className.startsWith(s)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * <p>
+     * getPackagesShouldNotBeInstrumented
+     * </p>
+     *
+     * @return the names of class packages EvoSuite is not going to instrument
+     */
+    public static String[] getPackagesShouldNotBeInstrumented() {
+        //explicitly blocking client projects such as specmate is only a
+        //temporary solution, TODO allow the user to specify
+        //packages that should not be instrumented
+        return new String[]{"java.", "javax.", "sun.", "org.evosuite", "org.exsyst",
+                "de.unisb.cs.st.testcarver", "de.unisb.cs.st.evosuite", "org.uispec4j",
+                "de.unisb.cs.st.specmate", "org.xml", "org.w3c",
+                "testing.generation.evosuite", "com.yourkit", "com.vladium.emma.", "daikon.",
+                "org.netbeans.lib.profiler", // VisualVM profiler
+                // Need to have these in here to avoid trouble with UnsatisfiedLinkErrors on Mac OS X and Java/Swing apps
+                "apple.", "com.apple.", "com.sun", "org.junit", "junit.framework",
+                "org.apache.xerces.dom3", "de.unisl.cs.st.bugex", "edu.uta.cse.dsc", "org.mozilla.javascript.gen.c",
+                "corina.cross.Single",  // I really don't know what is wrong with this class, but we need to exclude it
+                "org.slf4j",
+                /**
+                 * FIXME:
+                 * JavaEE libraries should be shaded, and not being here in blacklist, as many side-effects.
+                 * If there is need of having any blacklisted library which is also an EvoSuite
+                 * dependency, then we MUST modify classloader to always load the version of the SUT
+                 * (currently it seems we delegate to parent classloader, which gives EvoSuite's version?)
+                 *
+                 * */
+                //"org.hibernate","org.hsqldb","org.jboss", // used in the generated JUnit files to test JavaEE applications relying on database
+                "org.apache.commons.discovery.tools.DiscoverSingleton",
+                "org.apache.commons.discovery.resource.ClassLoaders",
+                "org.apache.commons.discovery.resource.classes.DiscoverClasses",
+                "org.apache.commons.logging.Log",// Leads to ExceptionInInitializerException when re-instrumenting classes that use a logger
+                "org.jcp.xml.dsig.internal.dom.", //Security exception in ExecutionTracer?
+                "com_cenqua_clover", "com.cenqua", //these are for Clover code coverage instrumentation
+                "net.sourceforge.cobertura", // cobertura code coverage instrumentation
+                "javafx.", // JavaFX crashes when instrumented
+                "ch.qos.logback", // Instrumentation makes logger events sent to the master un-serialisable
+                "org.apache.lucene.util.SPIClassIterator", "org.apache.lucene.analysis.util.AnalysisSPILoader", "org.apache.lucene.analysis.util.CharFilterFactory",
+                "org.apache.struts.util.MessageResources", "org.dom4j.DefaultDocumentFactory" // These classes all cause problems with re-instrumentation
+        };
+    }
+
+    public byte[] transformBytes(ClassLoader classLoader, String className,
+                                 ClassReader reader) {
+
+        String classNameWithDots = className.replace("/", ".");
+
+        if (!checkIfCanInstrument(classNameWithDots)) {
+            throw new IllegalArgumentException("Should not transform a shared class ("
+                    + classNameWithDots + ")! Load by parent (JVM) classloader.");
+        }
+
+        int asmFlags = ClassWriter.COMPUTE_FRAMES;
+        ClassWriter writer = new ComputeClassWriter(asmFlags);
+
+        ClassVisitor cv = writer;
+
+        if(RuntimeSettings.resetStaticState && !retransformingMode) {
+        		/*
+        		 * FIXME: currently reset does add a new method, but that does no work
+        		 * when retransformingMode :(
+        		 */
+            CreateClassResetClassAdapter resetClassAdapter = new CreateClassResetClassAdapter(cv, className);
+            resetClassAdapter.setRemoveFinalModifierOnStaticFields(true);
+            cv = resetClassAdapter;
+        }
+
+        if(RuntimeSettings.isUsingAnyMocking()) {
+            cv = new MethodCallReplacementClassAdapter(cv, className, !retransformingMode);
+        }
+
+        cv = new KillSwitchClassAdapter(cv);
+
+        if(RuntimeSettings.maxNumberOfIterationsPerLoop >= 0){
+        		cv = new LoopCounterClassAdapter(cv);
+        }
+
+        ClassNode cn = new AnnotatedClassNode();
+
+        int readFlags = ClassReader.SKIP_FRAMES;
+        reader.accept(cn, readFlags);
+        
+
+        cv = new JSRInlinerClassVisitor(cv);
+
+        try {
+            cn.accept(cv);
+        } catch (Throwable ex) {
+           logger.error("Error while instrumenting class "+className+": "+ex.getMessage(),ex);
+        }
+
+        return writer.toByteArray();
+    }
+
+}
