bb149f7c7b0f733fbdbcb0b85b16b1a8d2851671
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2015-07-16 22:17:57 +0100

    Re-add missing time delta stopping condition

---
+++ client/src/main/java/org/evosuite/testcase/statements/reflection/PrivateFieldStatement.java
@@ -0,0 +1,70 @@
+package org.evosuite.testcase.statements.reflection;
+
+import org.evosuite.ga.ConstructionFailedException;
+import org.evosuite.runtime.PrivateAccess;
+import org.evosuite.testcase.TestFactory;
+import org.evosuite.testcase.variable.ConstantValue;
+import org.evosuite.testcase.TestCase;
+import org.evosuite.testcase.variable.VariableReference;
+import org.evosuite.testcase.statements.MethodStatement;
+import org.evosuite.utils.generic.GenericClass;
+import org.evosuite.utils.generic.GenericMethod;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Statement representing the setting of a private field, which is done through reflection in the
+ * generated JUnit tests.
+ *
+ * Created by foo on 20/02/15.
+ */
+public class PrivateFieldStatement extends MethodStatement{
+
+	private static final long serialVersionUID = 5152490398872348493L;
+
+	private static Method setVariable;
+
+    static {
+        try {
+            //Class<T> klass, T instance, String fieldName, Object value
+            setVariable = PrivateAccess.class.getMethod("setVariable",Class.class, Object.class,String.class,Object.class);
+        } catch (NoSuchMethodException e) {
+            //should never happen
+            throw new RuntimeException("EvoSuite bug",e);
+        }
+    }
+
+    public PrivateFieldStatement(TestCase tc, Class<?> klass , String fieldName, VariableReference callee, VariableReference param)
+            throws NoSuchFieldException, IllegalArgumentException, ConstructionFailedException {
+        super(
+                tc,
+                new GenericMethod(setVariable,PrivateAccess.class),
+                null, //it is static
+                Arrays.asList(  // setVariable(Class<T> klass, T instance, String fieldName, Object value)
+                        new ConstantValue(tc,new GenericClass(Class.class),klass),  // Class<T> klass
+                        //new ClassPrimitiveStatement(tc,klass).getReturnValue(),  // Class<T> klass
+                        callee, // T instance
+                        new ConstantValue(tc,new GenericClass(String.class),fieldName),  // String fieldName
+                        param // Object value
+                )
+        );
+        List<GenericClass> parameterTypes = new ArrayList<GenericClass>();
+        parameterTypes.add(new GenericClass(klass));
+        this.method.setTypeParameters(parameterTypes);
+    }
+
+    @Override
+    public boolean mutate(TestCase test, TestFactory factory) {
+        // just for simplicity
+        return false;
+        //return super.mutate(test,factory); //tricky, as should do some restrictions
+    }
+    
+	@Override
+	public boolean isReflectionStatement() {
+		return true;
+	}
+}
---
+++ master/src/test/java/org/evosuite/jee/InjectionSystemTest.java
@@ -0,0 +1,86 @@
+package org.evosuite.jee;
+
+import com.examples.with.different.packagename.DependencyLibrary;
+import com.examples.with.different.packagename.jee.EntityManagerInjection;
+import com.examples.with.different.packagename.jee.MultipleDefaultInjections;
+import com.examples.with.different.packagename.jee.PostConstructorInjection;
+import org.evosuite.EvoSuite;
+import org.evosuite.Properties;
+import org.evosuite.SystemTest;
+import org.evosuite.ga.metaheuristics.GeneticAlgorithm;
+import org.evosuite.testsuite.TestSuiteChromosome;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Andrea Arcuri on 29/06/15.
+ */
+public class InjectionSystemTest extends SystemTest{
+
+    @Test
+    public void testPostConstructor(){
+
+        EvoSuite evosuite = new EvoSuite();
+
+        String targetClass = PostConstructorInjection.class.getCanonicalName();
+
+        Properties.TARGET_CLASS = targetClass;
+        Properties.CRITERION = new Properties.Criterion[]{Properties.Criterion.LINE};
+        Properties.JEE = true;
+        Properties.P_REFLECTION_ON_PRIVATE = 0.0;
+
+        String[] command = new String[] { "-generateSuite", "-class", targetClass };
+
+        Object result = evosuite.parseCommandLine(command);
+        GeneticAlgorithm<?> ga = getGAFromResult(result);
+        TestSuiteChromosome best = (TestSuiteChromosome) ga.getBestIndividual();
+        System.out.println("EvolvedTestSuite:\n" + best);
+
+        Assert.assertEquals("Non-optimal coverage: ", 1d, best.getCoverage(), 0.001);
+    }
+
+    @Test
+    public void testEntityManager(){
+
+        EvoSuite evosuite = new EvoSuite();
+
+        String targetClass = EntityManagerInjection.class.getCanonicalName();
+
+        Properties.TARGET_CLASS = targetClass;
+        Properties.CRITERION = new Properties.Criterion[]{Properties.Criterion.LINE};
+        Properties.JEE = true;
+        Properties.P_REFLECTION_ON_PRIVATE = 0.0;
+
+        String[] command = new String[] { "-generateSuite", "-class", targetClass };
+
+        Object result = evosuite.parseCommandLine(command);
+        GeneticAlgorithm<?> ga = getGAFromResult(result);
+        TestSuiteChromosome best = (TestSuiteChromosome) ga.getBestIndividual();
+        System.out.println("EvolvedTestSuite:\n" + best);
+
+        Assert.assertEquals("Non-optimal coverage: ", 1d, best.getCoverage(), 0.001);
+    }
+
+
+    @Test
+    public void testMultipleDefaultInjections(){
+
+        EvoSuite evosuite = new EvoSuite();
+
+        String targetClass = MultipleDefaultInjections.class.getCanonicalName();
+
+        Properties.TARGET_CLASS = targetClass;
+        Properties.CRITERION = new Properties.Criterion[]{Properties.Criterion.LINE};
+        Properties.JEE = true;
+        Properties.P_REFLECTION_ON_PRIVATE = 0.0;
+
+        String[] command = new String[] { "-generateSuite", "-class", targetClass };
+
+        Object result = evosuite.parseCommandLine(command);
+        GeneticAlgorithm<?> ga = getGAFromResult(result);
+        TestSuiteChromosome best = (TestSuiteChromosome) ga.getBestIndividual();
+        System.out.println("EvolvedTestSuite:\n" + best);
+
+        Assert.assertEquals("Non-optimal coverage: ", 1d, best.getCoverage(), 0.001);
+    }
+}
