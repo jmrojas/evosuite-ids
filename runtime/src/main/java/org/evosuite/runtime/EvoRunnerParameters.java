ef9b05726d6e3649c16f31c0fdf9ffe833a1979d
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2015-07-24 20:17:52 +0100

    Remove unused imports

---
+++ runtime/src/main/java/org/evosuite/runtime/EvoRunnerParameters.java
@@ -0,0 +1,28 @@
+package org.evosuite.runtime;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Annotation needed to pass parameters to EvoRunner
+ * 
+ * @author arcuri
+ *
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME) 
+public @interface EvoRunnerParameters {
+
+	boolean mockJVMNonDeterminism() default false; 
+
+    boolean useVFS() default false;
+
+    boolean useVNET() default false;
+
+    boolean resetStaticState() default false;
+    
+    boolean separateClassLoader() default false;
+
+    boolean useJEE() default false;
+}
