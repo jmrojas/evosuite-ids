347ebe1e310ed937f7baf425ca43b32ece072222
Andrea Arcuri <arcuri82@gmail.com>
2015-07-26 14:10:57 +0200

    fixed a constraint

---
+++ master/src/test/java/com/examples/with/different/packagename/localsearch/DseFoo.java
@@ -0,0 +1,18 @@
+package com.examples.with.different.packagename.localsearch;
+
+public class DseFoo {
+
+	private int x = 0;
+	
+	public DseFoo() {
+		System.out.println("Hello");
+	}
+	
+	public void inc() {
+		x++;
+	}
+	
+	public int getX() {
+		return x;
+	}
+}
