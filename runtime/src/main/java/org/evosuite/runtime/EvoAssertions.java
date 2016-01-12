bfb847810ea693f042246c35382f6d3189851c27
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2016-01-12 17:06:17 +0000

    Avoid assertThrownBy on flaky assertion types such as StackOverFlowException

---
+++ runtime/src/main/java/org/evosuite/runtime/EvoAssertions.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (C) 2010-2015 Gordon Fraser, Andrea Arcuri and EvoSuite
+ * contributors
+ *
+ * This file is part of EvoSuite.
+ *
+ * EvoSuite is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser Public License as published by the
+ * Free Software Foundation, either version 3.0 of the License, or (at your
+ * option) any later version.
+ *
+ * EvoSuite is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License along
+ * with EvoSuite. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.evosuite.runtime;
+
+import java.lang.annotation.Annotation;
+
+/**
+ * Created by Andrea Arcuri on 19/08/15.
+ */
+public class EvoAssertions {
+
+    public static void assertThrownBy(String sourceClass, Throwable t) throws AssertionError{
+        StackTraceElement el = t.getStackTrace()[0];
+
+        if(sourceClass==null){
+            return; //can this even happen?
+        }
+
+
+        String name = el.getClassName();
+        if(sourceClass.equals(name)){
+            return; //OK, same class, as expected
+        }
+
+        /*
+            Edge case: exception is thrown in a superclass method that is not overriden...
+            need to check hierarchy, interfaces included (as they might have code since Java 8)
+         */
+
+        Class<?> klass;
+
+        try {
+           klass = EvoAssertions.class.getClassLoader().loadClass(sourceClass);
+        } catch (ClassNotFoundException e) {
+            throw new AssertionError("Cannot load/analyze class "+sourceClass);
+        }
+
+        for(Annotation annotation : klass.getAnnotations()){
+            if(annotation.getClass().getName().equals(name)){
+                return;
+            }
+        }
+
+        while(klass != null){
+            klass = klass.getSuperclass();
+            if(klass != null && klass.getName().equals(name)){
+                return;
+            }
+        }
+
+        throw new AssertionError("Exception was not thrown in "+sourceClass +" but in "+el+": "+t);
+    }
+}
